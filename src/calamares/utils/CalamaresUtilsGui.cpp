3a3bf71054ecf4a20d73e3bd4fa354506731ac30
Teo Mrnjavac <teo@kde.org>
2014-07-01 11:48:10 +0200

    Added CalamaresUtilsGui

---
+++ src/calamares/utils/CalamaresUtilsGui.cpp
@@ -0,0 +1,41 @@
+/* === This file is part of Calamares - <http://github.com/calamares> ===
+ *
+ *   Copyright 2014, Teo Mrnjavac <teo@kde.org>
+ *
+ *   Calamares is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Calamares is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Calamares. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "CalamaresUtilsGui.h"
+
+#include <QLayout>
+
+namespace CalamaresUtils
+{
+
+void
+unmarginLayout( QLayout* layout )
+{
+    layout->setContentsMargins( 0, 0, 0, 0 );
+    layout->setMargin( 0 );
+    layout->setSpacing( 0 );
+
+    for ( int i = 0; i < layout->count(); i++ )
+    {
+        QLayout* childLayout = layout->itemAt( i )->layout();
+        if ( childLayout )
+            unmarginLayout( childLayout );
+    }
+}
+
+}
