6f3ec0170e08d1be6f7aafd59804dc8a09201160
Aurélien Gâteau <agateau@kde.org>
2014-07-02 15:59:34 +0200

    Argh, forgot to commit those files

---
+++ src/modules/partition/DeletePartitionJob.cpp
@@ -0,0 +1,49 @@
+/* === This file is part of Calamares - <http://github.com/calamares> ===
+ *
+ *   Copyright 2014, Aurélien Gâteau <agateau@kde.org>
+ *
+ *   Calamares is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Calamares is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Calamares. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <DeletePartitionJob.h>
+
+// CalaPM
+#include <core/device.h>
+#include <core/partition.h>
+#include <core/partitiontable.h>
+#include <fs/filesystem.h>
+
+DeletePartitionJob::DeletePartitionJob( Device* device, Partition* partition )
+    : m_device( device )
+    , m_partition( partition )
+{
+}
+
+QString
+DeletePartitionJob::prettyName()
+{
+    return tr( "Delete partition %1" ).arg( m_partition->partitionPath() );
+}
+
+void
+DeletePartitionJob::exec()
+{
+}
+
+void
+DeletePartitionJob::updatePreview()
+{
+    m_partition->parent()->remove( m_partition );
+    m_device->partitionTable()->updateUnallocated( *m_device );
+}
