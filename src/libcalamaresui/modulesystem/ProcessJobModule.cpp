1b4d62ef4d060945c6c8a717b1311615365de07f
Teo Mrnjavac <teo@kde.org>
2014-07-10 17:11:03 +0200

    Add ProcessJobModule for loading external commands.

---
+++ src/libcalamaresui/modulesystem/ProcessJobModule.cpp
@@ -0,0 +1,84 @@
+/* === This file is part of Calamares - <http://github.com/calamares> ===
+ *
+ *   Copyright 2014, Teo Mrnjavac <teo@kde.org>
+ *
+ *   Calamares is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Calamares is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Calamares. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "ProcessJobModule.h"
+
+#include "JobQueue.h"
+#include "ProcessJob.h"
+
+#include <QDir>
+
+#include <yaml-cpp/yaml.h>
+
+namespace Calamares {
+
+
+Module::Type
+ProcessJobModule::type() const
+{
+    return Job;
+}
+
+
+Module::Interface
+ProcessJobModule::interface() const
+{
+    return Process;
+}
+
+
+void
+ProcessJobModule::loadSelf()
+{
+    Calamares::job_ptr j = Calamares::job_ptr( new ProcessJob( m_command,
+                                                               m_workingPath,
+                                                               m_secondsTimeout ) );
+    JobQueue::instance()->enqueue( j );
+}
+
+
+void
+ProcessJobModule::initFrom( const YAML::Node& node )
+{
+    Module::initFrom( node );
+    QDir directory( location() );
+    m_workingPath = directory.absolutePath();
+
+    if ( node[ "command" ] )
+    {
+        m_command = QString::fromStdString( node[ "command" ].as< std::string >() );
+    }
+
+    m_secondsTimeout = 30;
+    if ( node[ "timeout" ] )
+    {
+        m_secondsTimeout = node[ "timeout" ].as< int >();
+    }
+}
+
+
+ProcessJobModule::ProcessJobModule()
+    : Module()
+{}
+
+
+ProcessJobModule::~ProcessJobModule()
+{}
+
+
+} // namespace Calamares
---
+++ src/libcalamaresui/modulesystem/ProcessJobModule.h
@@ -0,0 +1,51 @@
+/* === This file is part of Calamares - <http://github.com/calamares> ===
+ *
+ *   Copyright 2014, Teo Mrnjavac <teo@kde.org>
+ *
+ *   Calamares is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Calamares is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Calamares. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef CALAMARES_PROCESSJOBMODULE_H
+#define CALAMARES_PROCESSJOBMODULE_H
+
+#include "Module.h"
+
+#include "UiDllMacro.h"
+
+namespace Calamares {
+
+class UIDLLEXPORT ProcessJobModule : public Module
+{
+public:
+    Type type() const override;
+    Interface interface() const override;
+
+    void loadSelf() override;
+
+protected:
+    void initFrom( const YAML::Node &node ) override;
+
+private:
+    friend class Module;
+    explicit ProcessJobModule();
+    virtual ~ProcessJobModule();
+
+    QString m_command;
+    QString m_workingPath;
+    int m_secondsTimeout;
+};
+
+} // namespace Calamares
+
+#endif // CALAMARES_PROCESSJOBMODULE_H
