422e78236b7d9c2e63d2b712ce1d48243672db55
Teo Mrnjavac <teo@kde.org>
2014-07-28 17:57:53 +0200

    Make fileCopy in unsquashfs a single function

--- src/modules/unsquashfs/main.py
+++ src/modules/unsquashfs/main.py
@@ -18,16 +18,57 @@
 #   along with Calamares. If not, see <http://www.gnu.org/licenses/>.
 
 import os
+import re
 import shutil
 import subprocess
+import sys
 import tempfile
 from collections import namedtuple
 
 from libcalamares import *
-from filecopy import FileCopy
 
 UnpackEntry = namedtuple( 'UnpackEntry', [ 'source', 'destination', 'sourceDir' ] )
 UnpackStatusEntry = namedtuple( 'UnpackStatusEntry', [ 'copied', 'total' ] )
+
+ON_POSIX = 'posix' in sys.builtin_module_names
+
+
+def fileCopy( source, dest, progress_cb ):
+    # Environment used for executing rsync properly
+    # Setting locale to C (fix issue with tr_TR locale)
+    at_env = os.environ
+    at_env["LC_ALL"] = "C"
+
+    process = subprocess.Popen( [ 'rsync', '-ar', '--progress', source, dest ],
+                                env=self.at_env,
+                                bufsize=1,
+                                stdout=subprocess.PIPE,
+                                close_fds=ON_POSIX )
+
+    for line in iter( self.process.stdout.readline, b'' ):
+        # small comment on this regexp.
+        # rsync outputs three parameters in the progress.
+        # xfer#x => i try to interpret it as 'file copy try no. x'
+        # to-check=x/y, where:
+        #  - x = number of files yet to be checked
+        #  - y = currently calculated total number of files.
+        # but if you're copying directory with some links in it, the xfer# might not be a
+        # reliable counter. ( for one increase of xfer, many files may be created)
+        # In case of manjaro, we pre-compute the total number of files.
+        # therefore we can easily subtract x from y in order to get real files copied / processed count.
+        m = re.findall( r'xfr#(\d+), ir-chk=(\d+)/(\d+)', line.decode() )
+        if m:
+            # we've got a percentage update
+            num_files_remaining = int(m[0][1])
+            num_files_total_local = int(m[0][2])
+            # adjusting the offset so that progressbar can be continuesly drawn
+            num_files_copied = num_files_total_local - num_files_remaining
+
+
+            # I guess we're updating every 100 files...
+            if num_files_copied % 100 == 0:
+                progress_cb( num_files_copied )
+
 
 class UnsquashOperation:
     def __init__( self, unpack ):
@@ -78,11 +119,9 @@ class UnsquashOperation:
     def unsquashImage( self, entry ):
         subprocess.check_call( [ "mount", entry.source, entry.sourceDir, "-t", "squashfs", "-o", "loop" ] )
         try:
-            t = FileCopy( entry.sourceDir, entry.destination, self.reportProgress )
-            t.run()
+            fileCopy( entry.sourceDir, entry.destination, self.reportProgress )
         finally:
             subprocess.check_call( [ "umount", "-l", entry.sourceDir ] )
-
 
 
 def run():
