75e9fd6aa35179b86aa21138e495401267dd2780
Teo Mrnjavac <teo@kde.org>
2014-08-06 11:54:17 +0200

    Dracut module.

---
+++ src/modules/dracut/main.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+# === This file is part of Calamares - <http://github.com/calamares> ===
+#
+#   Copyright 2014, Philip Müller <philm@manjaro.org>
+#   Copyright 2014, Teo Mrnjavac <teo@kde.org>
+#
+#   Calamares is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   Calamares is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with Calamares. If not, see <http://www.gnu.org/licenses/>.
+
+import libcalamares
+from libcalamares.utils import check_chroot_call
+
+def run_dracut():
+    kernelname = libcalamares.job.configuration['kernel']
+    check_chroot_call(['dracut', '-f'])
+
+def run():
+    run_dracut()
+    return None
