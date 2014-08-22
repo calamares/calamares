0c92aff456b34e482293f0f9b325c02d2333989a
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2014-08-22 15:55:47 +0200

    [DO NOT MERGE] add hwdetect module to detect efi/bios

---
+++ src/modules/hwdetect/main.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+# === This file is part of Calamares - <http://github.com/calamares> ===
+#
+#   Copyright 2014, Daniel Hillenbrand <codeworkx@bbqlinux.org>
+#
+#   Calamares is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   Calamares is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with Calamares. If not, see <http://www.gnu.org/licenses/>.
+
+import os
+
+import libcalamares
+
+def detect_firmware_type():
+    # Check for EFI variables support
+    if(os.path.exists("/sys/firmware/efi/efivars")):
+        fw_type = 'efi'
+    else:
+        fw_type = 'bios'
+
+    libcalamares.globalstorage.insert("firmwareType", fw_type)
+    libcalamares.utils.debug("Firmware type: {!s}".format(fw_type))
+
+def run():
+    detect_firmware_type()
+    return None
