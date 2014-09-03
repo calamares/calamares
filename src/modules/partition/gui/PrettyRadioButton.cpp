d39e77f7e8635b435b527bc3d5bc5a5334ad567f
Teo Mrnjavac <teo@kde.org>
2014-09-03 18:04:10 +0200

    New PrettyRadioButton, an alternative to QRadioButton with rich text.

---
+++ src/modules/partition/gui/PrettyRadioButton.cpp
@@ -0,0 +1,85 @@
+/* === This file is part of Calamares - <http://github.com/calamares> ===
+ *
+ *   Copyright 2014, Teo Mrnjavac <teo@kde.org>
+ *
+ *   Calamares is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Calamares is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Calamares. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "PrettyRadioButton.h"
+
+#include "utils/CalamaresUtilsGui.h"
+#include "widgets/ClickableLabel.h"
+
+#include <QLabel>
+#include <QBoxLayout>
+
+
+PrettyRadioButton::PrettyRadioButton( QWidget* parent )
+    : QWidget( parent )
+{
+    QHBoxLayout* mainLayout = new QHBoxLayout;
+    setLayout( mainLayout );
+
+    m_radio = new QRadioButton;
+    m_label = new ClickableLabel;
+
+    connect( m_label, &ClickableLabel::clicked,
+             m_radio, &QRadioButton::click );
+    m_label->setBuddy( m_radio );
+
+    m_label->setWordWrap( true );
+    m_label->setSizePolicy( QSizePolicy::Expanding, QSizePolicy::Preferred );
+    QMargins labelMargins = m_label->contentsMargins();
+    labelMargins.setLeft( labelMargins.left() + CalamaresUtils::defaultFontHeight() * 0.8 );
+    m_label->setContentsMargins( labelMargins );
+
+    mainLayout->addWidget( m_radio );
+    mainLayout->addWidget( m_label );
+    mainLayout->setContentsMargins( 0, 0, 0, 0 );
+}
+
+
+void
+PrettyRadioButton::setText( const QString& text )
+{
+    m_label->setText( text );
+}
+
+
+void
+PrettyRadioButton::setIconSize( const QSize& size )
+{
+    m_radio->setIconSize( size );
+}
+
+
+void
+PrettyRadioButton::setIcon( const QIcon& icon )
+{
+    m_radio->setIcon( icon );
+}
+
+
+QSize
+PrettyRadioButton::iconSize()
+{
+    return m_radio->iconSize();
+}
+
+
+QRadioButton*
+PrettyRadioButton::buttonWidget()
+{
+    return m_radio;
+}
