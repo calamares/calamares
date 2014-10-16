b8b2c2b735f4d0e4cc0e81753b10276643a3b868
Rohan Garg <rohan@kde.org>
2014-10-16 19:00:44 +0200

    Properly break lines in Python

--- src/modules/grubcfg/main.py
+++ src/modules/grubcfg/main.py
@@ -30,8 +30,8 @@ def modify_grub_default(partitions, root_mount_point, distributor):
     swap_uuid = ""
 
     if not os.path.exists(default_dir):
-        return ("Directory does not exist", "The directory {} does not exist on
-        the target".format(default_dir))
+        return ("Directory does not exist", "The directory {} does not exist on "
+        "the target".format(default_dir))
     if os.path.exists(plymouth_bin):
         use_splash = "splash"
 
