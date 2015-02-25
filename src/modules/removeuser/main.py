5a1567f4f53fb8605284dc4dea3f00143f98f9f1
Teo Mrnjavac <teo@kde.org>
2015-02-25 17:37:25 +0100

    removeuser job module, this time in Python.

---
+++ src/modules/removeuser/main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# === This file is part of Calamares - <http://github.com/calamares> ===
+#
+#   Copyright 2015, Teo Mrnjavac <teo@kde.org>
+#
+#   Calamares is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   Calamares is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with Calamares. If not, see <http://www.gnu.org/licenses/>.
+
+import subprocess
+import libcalamares
+
+
+def run():
+    """ Remove live user from target system """
+
+    username = libcalamares.job.configuration[("username")]
+    try:
+        libcalamares.utils.check_chroot_call(["userdel", "-f", "-r", username])
+    except subprocess.CalledProcessError as e:
+        libcalamares.utils.debug( "Cannot remove user. " +
+                                  "userdel terminated with exit code {}.".format(e.returncode))
+
+    return None
