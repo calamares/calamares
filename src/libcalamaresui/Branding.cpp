be29755230930f5d81ba107d248dadc65bfcb346
demmm <anke62@gmail.com>
2015-03-19 10:32:14 -0400

    add URL option in greeting page URL is a variable set in branding.desc, distros cna link to a Known Issues page text for the URL is also possible to set in branding, known issues is default advantage is any text possible as distros see fit disadvantage, not part of translated strings if most agree, Known Issues can be set as hard coded text, not in branding.desc

--- src/libcalamaresui/Branding.cpp
+++ src/libcalamaresui/Branding.cpp
@@ -1,6 +1,6 @@
 /* === This file is part of Calamares - <http://github.com/calamares> ===
  *
- *   Copyright 2014, Teo Mrnjavac <teo@kde.org>
+ *   Copyright 2014-2015, Teo Mrnjavac <teo@kde.org>
  *
  *   Calamares is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -52,7 +52,9 @@ QStringList Branding::s_stringEntryStrings =
     "versionedName",
     "shortVersionedName",
     "shortProductName",
-    "bootloaderEntryName"
+    "bootloaderEntryName",
+    "productURL",
+    "urlText"
 };
 
 
