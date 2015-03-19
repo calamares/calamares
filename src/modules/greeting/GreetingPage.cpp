d750885e759885e754d33e6318f5475a6b23a5fe
demmm <anke62@gmail.com>
2015-03-19 10:44:34 -0400

    Revert "add URL option in greeting page"

--- src/modules/greeting/GreetingPage.cpp
+++ src/modules/greeting/GreetingPage.cpp
@@ -1,6 +1,6 @@
 /* === This file is part of Calamares - <http://github.com/calamares> ===
  *
- *   Copyright 2014-2015, Teo Mrnjavac <teo@kde.org>
+ *   Copyright 2014, Teo Mrnjavac <teo@kde.org>
  *
  *   Calamares is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -123,19 +123,11 @@ GreetingPage::GreetingPage( QWidget* parent )
     CALAMARES_RETRANSLATE(
         ui->mainText->setText( tr( "<h1>Welcome to the %1 installer.</h1><br/>"
                              "This program will ask you some questions and "
-                             "set up %2 on your computer." 
-                             " Remember, this installer is still in an early stage.<br/>"
-                             "Not implemented yet is LUKS/LVM or RAID and<br/>"
-                             "using one of the automated partitioning options for UEFI installs.<br/>" 
-                             "For <b>GPT partioning on BIOS</b> systems and other instructions, read<a href=\"%3\"> %4 </a>" )
+                             "set up %2 on your computer." )
                          .arg( Calamares::Branding::instance()->
                                string( Calamares::Branding::VersionedName ) )
                          .arg( Calamares::Branding::instance()->
-                               string( Calamares::Branding::ProductName ) ) 
-                         .arg( Calamares::Branding::instance()->
-                               string( Calamares::Branding::ProductURL ) )
-                         .arg( Calamares::Branding::instance()->
-                               string( Calamares::Branding::UrlText ) ) );
+                               string( Calamares::Branding::ProductName ) ) );
     )
 
     ui->aboutButton->setIcon( CalamaresUtils::defaultPixmap( CalamaresUtils::Information,
