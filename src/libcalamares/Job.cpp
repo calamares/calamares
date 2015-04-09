d45d43616117f12c6ad82295ce863616ad2b7d64
Teo Mrnjavac <teo@kde.org>
2015-04-09 15:03:47 +0200

    Add optional prettyDescription to Job.

--- src/libcalamares/Job.cpp
+++ src/libcalamares/Job.cpp
@@ -1,6 +1,6 @@
 /* === This file is part of Calamares - <http://github.com/calamares> ===
  *
- *   Copyright 2014, Teo Mrnjavac <teo@kde.org>
+ *   Copyright 2014-2015, Teo Mrnjavac <teo@kde.org>
  *
  *   Calamares is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -85,6 +85,13 @@ Job::Job( QObject* parent )
 
 Job::~Job()
 {}
+
+
+QString
+Job::prettyDescription() const
+{
+    return QString();
+}
 
 
 } // namespace Calamares
