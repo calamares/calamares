b15ccdfeefaa4efc5fe2998fa64340635177e6f2
Teo Mrnjavac <teo@kde.org>
2015-04-16 18:20:29 +0200

    Year too.

--- src/modules/greeting/GreetingPage.cpp
+++ src/modules/greeting/GreetingPage.cpp
@@ -147,7 +147,7 @@ GreetingPage::GreetingPage( QWidget* parent )
                                 "<h1>%1</h1><br/>"
                                 "<strong>%2<br/>"
                                 "for %3</strong><br/><br/>"
-                                "Copyright 2014 Teo Mrnjavac &lt;teo@kde.org&gt;<br/>"
+                                "Copyright 2014-2015 Teo Mrnjavac &lt;teo@kde.org&gt;<br/>"
                                 "Thanks to: Anke Boersma, Aurélien Gâteau, Kevin Kofler, Philip Müller, "
                                 "Pier Luigi Fiorini and Rohan Garg.<br/><br/>"
                                 "<a href=\"http://calamares.io/\">Calamares</a> "
