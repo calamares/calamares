11f5b729a840b936d764e648b7559c3966da3c44
Teo Mrnjavac <teo@kde.org>
2015-06-14 01:10:50 +0200

    Avoid dereferencing nullptr.

--- src/libcalamaresui/modulesystem/ModuleManager.cpp
+++ src/libcalamaresui/modulesystem/ModuleManager.cpp
@@ -101,7 +101,8 @@ ModuleManager::loadModules( Phase phase )
     {
         foreach ( const QString& moduleName, Settings::instance()->modules( phase ) )
         {
-            if ( !m_availableModules.contains( moduleName ) )
+            if ( !m_availableModules.contains( moduleName ) ||
+                 !m_availableModules.value( moduleName ) )
             {
                 cDebug() << "Module" << moduleName << "not found in module search paths."
                          << "\nCalamares will now quit.";
