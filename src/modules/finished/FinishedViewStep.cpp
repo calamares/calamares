33ab4ee6630769dda9ac77b73c15872f12b1c80a
Teo Mrnjavac <teo@kde.org>
2015-11-06 14:57:01 +0100

    Enable Restart Now checkBox if required.

--- src/modules/finished/FinishedViewStep.cpp
+++ src/modules/finished/FinishedViewStep.cpp
@@ -113,10 +113,9 @@ FinishedViewStep::setConfigurationMap( const QVariantMap& configurationMap )
     {
         bool restartNowEnabled = configurationMap.value( "restartNowEnabled" ).toBool();
 
+        m_widget->setRestartNowEnabled( restartNowEnabled );
         if ( restartNowEnabled )
         {
-            m_widget->setRestartNowEnabled( restartNowEnabled );
-
             if ( configurationMap.contains( "restartNowChecked" ) &&
                  configurationMap.value( "restartNowChecked" ).type() == QVariant::Bool &&
                  configurationMap.contains( "restartNowCommand" ) &&
