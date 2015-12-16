e62665bfaeedc9bc8b7e48a1d97d8041a3244229
Teo Mrnjavac <teo@kde.org>
2015-12-16 15:47:11 +0100

    Add separate FileSystemTypeRole to PM, and output FS PrettyNames.

--- src/modules/partition/core/PartitionModel.h
+++ src/modules/partition/core/PartitionModel.h
@@ -71,6 +71,7 @@ public:
         IsFreeSpaceRole,
         IsPartitionNewRole,
         FileSystemLabelRole,
+        FileSystemTypeRole,
         OsproberNameRole,
         OsproberPathRole,
         OsproberCanBeResizedRole,
