5372245aedb2c9557cf7311ebe44890539f07544
Teo Mrnjavac <teo@kde.org>
2016-02-12 17:19:30 +0100

    Copyright statements.

--- src/modules/partition/gui/PartitionPage.cpp
+++ src/modules/partition/gui/PartitionPage.cpp
@@ -1,6 +1,7 @@
 /* === This file is part of Calamares - <http://github.com/calamares> ===
  *
  *   Copyright 2014, Aurélien Gâteau <agateau@kde.org>
+ *   Copyright 2015-2016, Teo Mrnjavac <teo@kde.org>
  *
  *   Calamares is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
