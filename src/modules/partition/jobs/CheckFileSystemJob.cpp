49cb6d304d89b30af8736b9816c7e6de1f67ddf1
Teo Mrnjavac <teo@kde.org>
2016-06-17 13:55:37 +0200

    Add a controlled number of retries to fsck, 2sec apart.

--- src/modules/partition/jobs/CheckFileSystemJob.cpp
+++ src/modules/partition/jobs/CheckFileSystemJob.cpp
@@ -1,6 +1,7 @@
 /* === This file is part of Calamares - <http://github.com/calamares> ===
  *
  *   Copyright 2014, Aurélien Gâteau <agateau@kde.org>
+ *   Copyright 2016, Teo Mrnjavac <teo@kde.org>
  *
  *   Calamares is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -24,6 +25,8 @@
 #include <kpmcore/core/partition.h>
 #include <kpmcore/fs/filesystem.h>
 #include <kpmcore/util/report.h>
+
+#include <QThread>
 
 CheckFileSystemJob::CheckFileSystemJob( Partition* partition )
     : PartitionJob( partition )
@@ -55,19 +58,26 @@ CheckFileSystemJob::exec()
 
     Report report( nullptr );
     bool ok = fs.check( report, partition()->partitionPath() );
-    if ( !ok )
+    int retries = 0;
+    const int MAX_RETRIES = 10;
+    while ( !ok )
     {
-        cDebug() << "Filesystem check failed for" << partition()->partitionPath()
-                 << ", retrying...";
+        cDebug() << "Partition" << partition()->partitionPath()
+                 << "might not be ready yet, retrying (" << ++retries
+                 << "/" << MAX_RETRIES << ") ...";
+        QThread::sleep( 2 /*seconds*/ );
         ok = fs.check( report, partition()->partitionPath() );
 
-        if ( !ok )
-            return Calamares::JobResult::error(
-                       tr( "The file system check on partition %1 failed." )
-                       .arg( partition()->partitionPath() ),
-                       report.toText()
-                   );
+        if ( retries == MAX_RETRIES )
+            break;
     }
+
+    if ( !ok )
+        return Calamares::JobResult::error(
+                   tr( "The file system check on partition %1 failed." )
+                   .arg( partition()->partitionPath() ),
+                   report.toText()
+               );
 
     return Calamares::JobResult::ok();
 }
