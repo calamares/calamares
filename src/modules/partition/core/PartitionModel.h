1cfdc8044cca98846b01fc52a6af99b6f54d30f9
shainer <syn.shainer@gmail.com>
2016-07-23 22:28:22 +0100

    Display current filesystem label as a column in the main partition view.

--- src/modules/partition/core/PartitionModel.h
+++ src/modules/partition/core/PartitionModel.h
@@ -79,6 +79,7 @@ public:
     {
         NameColumn,
         FileSystemColumn,
+        FileSystemLabelColumn,
         MountPointColumn,
         SizeColumn,
         ColumnCount  // Must remain last
