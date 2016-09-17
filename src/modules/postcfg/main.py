2b00d28de29d0b6e5e4a59092a7efffad8c670d7
udeved <artoo@manjaro.org>
2016-09-17 23:03:55 +0200

    postcfg: only attempt to rank mirrors if hasInternet clean up imports

--- src/modules/postcfg/main.py
+++ src/modules/postcfg/main.py
@@ -21,47 +21,45 @@
 
 import libcalamares
 
-import os
-import shutil
-import distutils.dir_util
-
+from shutil import copy2
+from distutils.dir_util import copy_tree
 from os.path import join, exists
-from libcalamares.utils import target_env_call, check_target_env_call
+from libcalamares.utils import target_env_call
 
 class ConfigController:
     def __init__(self):
-        self.__root = libcalamares.globalstorage.value( "rootMountPoint" )
+        self.__root = libcalamares.globalstorage.value("rootMountPoint")
         self.__keyrings = libcalamares.job.configuration.get('keyrings', [])
-        
+
     @property
     def root(self):
         return self.__root
-    
+
     @property
     def keyrings(self):
         return self.__keyrings
-      
+
     def init_keyring(self):
-        check_target_env_call(["pacman-key", "--init"])
+        target_env_call(["pacman-key", "--init"])
 
     def populate_keyring(self):
-        check_target_env_call(["pacman-key", "--populate"] + self.keyrings)
-        
+        target_env_call(["pacman-key", "--populate"] + self.keyrings)
+
     def terminate(self, proc):
         target_env_call(['killall', '-9', proc])
-        
+
     def copy_file(self, file):
         if exists("/" + file):
-            shutil.copy2("/" + file, join(self.root, file))
+            copy2("/" + file, join(self.root, file))
 
     def copy_folder(self, source, target):
         if exists("/" + source):
-            distutils.dir_util.copy_tree("/" + source, join(self.root, target))
-    
+            copy_tree("/" + source, join(self.root, target))
+
     def remove_pkg(self, pkg, path):
         if exists(join(self.root, path)):
             target_env_call(['pacman', '-R', '--noconfirm', pkg])
-    
+
     def run(self):
         self.init_keyring()
         self.populate_keyring()
@@ -71,27 +69,28 @@ class ConfigController:
 
         # Generate mirror list
         if exists(join(self.root, "usr/bin/pacman-mirrors")):
-            target_env_call(["pacman-mirrors", "-g"])
+            if libcalamares.globalstorage.value("hasInternet"):
+                target_env_call(["pacman-mirrors", "-g"])
         else:
             self.copy_file('etc/pacman.d/mirrorlist')
 
         # Copy skel to root
         self.copy_folder('etc/skel', 'root')
-        
+
         # Workaround for pacman-key bug FS#45351 https://bugs.archlinux.org/task/45351
         # We have to kill gpg-agent because if it stays around we can't reliably unmount
         # the target partition.
         self.terminate('gpg-agent')
-        
+
         # Update grub.cfg
         if exists(join(self.root, "usr/bin/update-grub")) and libcalamares.globalstorage.value("bootLoader") is not None:
             target_env_call(["update-grub"])
-        
+
         return None
-    
+
 def run():
     """ Misc postinstall configurations """
 
     config = ConfigController()
-    
+
     return config.run()
