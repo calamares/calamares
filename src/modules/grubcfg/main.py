eca367b9c0947e827d6ab6971bc0e2aed14c070c
V3n3RiX <venerix@rogentos.ro>
2016-10-19 16:41:35 +0100

    Only write rd.luks.uuid if dracut is present and leave other initramfs generators clean (#266)
    * add rd.luks.uuid to GRUB_CMDLINE (fixes unbootable system with dracut --nohostonly, and doesn't affect any other initramfs generators)
    
    * typo
    
    * only write rd.luks.uuid line if dracut is present, and leave other initramfs generators alone :D
    
    * add missing = operator

--- src/modules/grubcfg/main.py
+++ src/modules/grubcfg/main.py
@@ -36,6 +36,7 @@ def modify_grub_default(partitions, root_mount_point, distributor):
     default_grub = os.path.join(default_dir, "grub")
     distributor_replace = distributor.replace("'", "'\\''")
     plymouth_bin = libcalamares.utils.target_env_call(["sh", "-c", "which plymouth"])
+    dracut_bin = libcalamares.utils.target_env_call(["sh", "-c", "which dracut"])
     use_splash = ""
     swap_uuid = ""
 
@@ -46,17 +47,23 @@ def modify_grub_default(partitions, root_mount_point, distributor):
 
     cryptdevice_params = []
 
-    for partition in partitions:
-        if partition["fs"] == "linuxswap":
-            swap_uuid = partition["uuid"]
+    if dracut_bin == 0:
+        for partition in partitions:
+            if partition["fs"] == "linuxswap":
+                swap_uuid = partition["uuid"]
 
-        if partition["mountPoint"] == "/" and "luksMapperName" in partition:
-            cryptdevice_params = [
-                "cryptdevice=UUID={!s}:{!s}".format(partition["luksUuid"],
-                                                    partition["luksMapperName"]),
-                "root=/dev/mapper/{!s}".format(partition["luksMapperName"]),
-                # Fix for unbootable system with dracut --nohostonly
-                "rd.luks.uuid={!s}".format(partition["luksUuid"])
+            if partition["mountPoint"] == "/" and "luksMapperName" in partition:
+                cryptdevice_params = ["rd.luks.uuid={!s}".format(partition["luksUuid"])]
+    else:
+        for partition in partitions:
+            if partition["fs"] == "linuxswap":
+                swap_uuid = partition["uuid"]
+
+            if partition["mountPoint"] == "/" and "luksMapperName" in partition:
+                cryptdevice_params = [
+                    "cryptdevice=UUID={!s}:{!s}".format(partition["luksUuid"],
+                                                        partition["luksMapperName"]),
+                    "root=/dev/mapper/{!s}".format(partition["luksMapperName"])
             ]
 
     kernel_params = ["quiet"]
