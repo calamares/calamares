7d2bd264b6025e84f983de5c83f79db372c93fac
Teo Mrnjavac <teo@kde.org>
2017-01-17 18:03:51 +0100

    Useless check is useless.

--- src/modules/bootloader/main.py
+++ src/modules/bootloader/main.py
@@ -282,8 +282,6 @@ def run():
 
     :return:
     """
-    if libcalamares.globalstorage.value("bootLoader") is None:
-        return None
 
     fw_type = libcalamares.globalstorage.value("firmwareType")
     prepare_bootloader(fw_type)
