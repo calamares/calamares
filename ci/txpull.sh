97ded0ec4808b7f1d5efd1a97ac7a27f26fd10ba
Adriaan de Groot <groot@kde.org>
2017-06-28 08:09:22 -0400

    i18n: see if Transifex sync uses these scripts.

--- ci/txpull.sh
+++ ci/txpull.sh
@@ -11,7 +11,7 @@ git config --global http.sslVerify false
 export QT_SELECT=5
 tx pull --force --source --all
 git add --verbose lang/calamares*.ts
-git commit --author='Calamares CI <teo@kde.org>' --message='[core] Automatic merge of Transifex translations' | true
+git commit --author='Calamares CI <groot@kde.org>' --message='[core] Automatic merge of Transifex translations' | true
 
 for MODULE_DIR in `find src/modules -maxdepth 1 -mindepth 1 -type d`; do
   FILES=(${MODULE_DIR}/*.py)
@@ -23,7 +23,7 @@ for MODULE_DIR in `find src/modules -maxdepth 1 -mindepth 1 -type d`; do
         msgfmt -o ${POFILE/.po/.mo} $POFILE
       done
       git add --verbose ${MODULE_DIR}/lang/*
-      git commit --author='Calamares CI <teo@kde.org>' --message="[${MODULE_NAME}] Automatic merge of Transifex translations" | true
+      git commit --author='Calamares CI <groot@kde.org>' --message="[${MODULE_NAME}] Automatic merge of Transifex translations" | true
     fi
   fi
 done
