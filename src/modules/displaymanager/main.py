calamares/calamares

commit c8fd11b103b10d2b1a78ee7bde3dd8cc7ebee869

Replace scandir with listdir, simplify loop
FIXES: ??

src/modules/displaymanager/main.py
@@ -480,12 +480,9 @@ def run():
                 )
 
             if (os.path.exists(greeter_path)):
-                greeter_configured = False
-
                 # configure first found lightdm-greeter
-                for entry in os.scandir(greeter_path):
-                    if entry.name.endswith('.desktop') \
-                            and not greeter_configured:
+                for entry in os.listdir(greeter_path):
+                    if entry.name.endswith('.desktop'):
                         greeter = entry.name.split('.')[0]
                         libcalamares.utils.debug(
                             "found greeter {!s}".format(greeter)
@@ -500,9 +497,8 @@ def run():
                         libcalamares.utils.debug(
                             "{!s} configured as greeter.".format(greeter)
                             )
-                        greeter_configured = True
-
-                if not greeter_configured:
+                        break
+                else:
                     return ("No lightdm greeter installed.")
         else:
             libcalamares.utils.debug("lightdm selected but not installed")

commit d20285d4f20a3f7ff76ea145b50bdec0b7d0ef2f

[modules] displaymanager: try to fix regression introduced with c8fd11b

src/modules/displaymanager/main.py
@@ -483,8 +483,8 @@ def run():
             if (os.path.exists(greeter_path)):
                 # configure first found lightdm-greeter
                 for entry in os.listdir(greeter_path):
-                    if entry.name.endswith('.desktop'):
-                        greeter = entry.name.split('.')[0]
+                    if entry.endswith('.desktop'):
+                        greeter = entry.split('.')[0]
                         libcalamares.utils.debug(
                             "found greeter {!s}".format(greeter)
                             )