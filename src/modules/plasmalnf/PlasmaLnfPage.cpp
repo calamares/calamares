98d758b4bed05ad37dc7e33a840d7eb57bf6c095
Adriaan de Groot <groot@kde.org>
2017-12-03 07:08:54 -0500

    [plasmalnf] Hammer out the most primitive UI

---
+++ src/modules/plasmalnf/PlasmaLnfPage.cpp
@@ -0,0 +1,47 @@
+/* === This file is part of Calamares - <http://github.com/calamares> ===
+ *
+ *   Copyright 2017, Adriaan de Groot <groot@kde.org>
+ *
+ *   Calamares is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Calamares is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Calamares. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "PlasmaLnfPage.h"
+
+#include "ui_page_plasmalnf.h"
+
+#include "Branding.h"
+#include "JobQueue.h"
+#include "GlobalStorage.h"
+#include "utils/Logger.h"
+#include "utils/CalamaresUtilsGui.h"
+#include "utils/Retranslator.h"
+#include "ViewManager.h"
+
+#include <QButtonGroup>
+#include <QDesktopServices>
+#include <QLabel>
+
+PlasmaLnfPage::PlasmaLnfPage(QWidget *parent)
+    : QWidget( parent )
+    , ui( new Ui::PlasmaLnfPage )
+{
+    using StringEntry = Calamares::Branding::StringEntry;
+
+    ui->setupUi( this );
+    CALAMARES_RETRANSLATE(
+        ui->retranslateUi( this );
+        ui->generalExplanation->setText( tr( "Please choose a look-and-feel for the KDE Plasma Desktop, below." ) );
+    )
+}
+
---
+++ src/modules/plasmalnf/PlasmaLnfViewStep.cpp
@@ -0,0 +1,121 @@
+/* === This file is part of Calamares - <http://github.com/calamares> ===
+ *
+ *   Copyright 2017, Adriaan de Groot <groot@kde.org>
+ *
+ *   Calamares is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Calamares is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Calamares. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "JobQueue.h"
+#include "GlobalStorage.h"
+#include "utils/Logger.h"
+#include "utils/CalamaresUtils.h"
+#include "utils/CalamaresUtilsSystem.h"
+
+#include "PlasmaLnfJob.h"
+#include "PlasmaLnfPage.h"
+#include "PlasmaLnfViewStep.h"
+
+#include <QDesktopServices>
+#include <QVariantMap>
+
+CALAMARES_PLUGIN_FACTORY_DEFINITION( PlasmaLnfViewStepFactory, registerPlugin<PlasmaLnfViewStep>(); )
+
+PlasmaLnfViewStep::PlasmaLnfViewStep( QObject* parent )
+    : Calamares::ViewStep( parent )
+    , m_widget( new PlasmaLnfPage )
+{
+    emit nextStatusChanged( false );
+}
+
+
+PlasmaLnfViewStep::~PlasmaLnfViewStep()
+{
+    if ( m_widget && m_widget->parent() == nullptr )
+        m_widget->deleteLater();
+}
+
+
+QString
+PlasmaLnfViewStep::prettyName() const
+{
+    return tr( "Look-and-Feel" );
+}
+
+
+QWidget*
+PlasmaLnfViewStep::widget()
+{
+    return m_widget;
+}
+
+
+void
+PlasmaLnfViewStep::next()
+{
+    emit done();
+}
+
+
+void
+PlasmaLnfViewStep::back()
+{}
+
+
+bool
+PlasmaLnfViewStep::isNextEnabled() const
+{
+    return true;
+}
+
+
+bool
+PlasmaLnfViewStep::isBackEnabled() const
+{
+    return true;
+}
+
+
+bool
+PlasmaLnfViewStep::isAtBeginning() const
+{
+    return true;
+}
+
+
+bool
+PlasmaLnfViewStep::isAtEnd() const
+{
+    return true;
+}
+
+
+void PlasmaLnfViewStep::onLeave()
+{
+}
+
+
+Calamares::JobList
+PlasmaLnfViewStep::jobs() const
+{
+    Calamares::JobList l;
+
+    cDebug() << "Creating Plasma LNF jobs ..";
+    return l;
+}
+
+
+void
+PlasmaLnfViewStep::setConfigurationMap( const QVariantMap& configurationMap )
+{
+}
