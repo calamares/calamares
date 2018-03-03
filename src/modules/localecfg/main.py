58952b685ca81ee6e3090deb0ae85add96f6ef6a
AlmAck <gluca86@gmail.com>
2018-03-03 14:30:08 +0100

    the first character must be a comment
    to original code would match both lines:
    zen_US.UTF-8 UTF-8
    #en_US.UTF-8 UTF-8
    
    introducing "#" should check only the commented lines

--- src/modules/localecfg/main.py
+++ src/modules/localecfg/main.py
@@ -65,15 +65,12 @@ def run():
         with open("{!s}/etc/locale.gen".format(install_path), "w") as gen:
             for line in text:
                 # always enable en_US
-                if line.startswith(en_us_locale, 1):
+                if line.startswith("#" + en_us_locale):
                     # uncomment line
                     line = line[1:].lstrip()
 
                 for locale_value in locale_values:
-                    # check the locale value starting from
-                    # the second index because we expect that
-                    # the first one is a '#'
-                    if line.startswith(locale_value, 1):
+                    if line.startswith("#" + locale_value):
                         # uncomment line
                         line = line[1:].lstrip()
 
