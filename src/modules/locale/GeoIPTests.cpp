eea421f499f043524e1732dca53d47f7d5580e6c
Adriaan de Groot <groot@kde.org>
2018-04-12 16:05:02 -0400

    [locale] Add tests for GeoIP handlers
     - One sample JSON result
     - Two sample XML results

---
+++ src/modules/locale/GeoIPTests.cpp
@@ -0,0 +1,98 @@
+/* === This file is part of Calamares - <http://github.com/calamares> ===
+ *
+ *   Copyright 2018, Adriaan de Groot <groot@kde.org>
+ *
+ *   Calamares is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Calamares is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Calamares. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "GeoIPTests.h"
+
+#include "GeoIPFreeGeoIP.h"
+#ifdef HAVE_XML
+#include "GeoIPXML.h"
+#endif
+
+#include <QtTest/QtTest>
+
+QTEST_GUILESS_MAIN( GeoIPTests )
+
+GeoIPTests::GeoIPTests()
+{
+}
+
+GeoIPTests::~GeoIPTests()
+{
+}
+
+void
+GeoIPTests::initTestCase()
+{
+}
+
+void
+GeoIPTests::testJSON()
+{
+    static const char data[] =
+        "{\"time_zone\":\"Europe/Amsterdam\"}";
+
+    FreeGeoIP handler;
+    auto tz = handler.processReply( data );
+
+    QCOMPARE( tz.first, QLatin1String( "Europe" ) );
+    QCOMPARE( tz.second, QLatin1String( "Amsterdam" ) );
+}
+
+void
+GeoIPTests::testXML()
+{
+    static const char data[] =
+        R"(<Response>
+  <Ip>85.150.1.1</Ip>
+  <Status>OK</Status>
+  <CountryCode>NL</CountryCode>
+  <CountryCode3>NLD</CountryCode3>
+  <CountryName>Netherlands</CountryName>
+  <RegionCode>None</RegionCode>
+  <RegionName>None</RegionName>
+  <City>None</City>
+  <ZipPostalCode/>
+  <Latitude>50.0</Latitude>
+  <Longitude>4.0</Longitude>
+  <AreaCode>0</AreaCode>
+  <TimeZone>Europe/Amsterdam</TimeZone>
+</Response>)";
+
+#ifdef HAVE_XML
+    XMLGeoIP handler;
+    auto tz = handler.processReply( data );
+
+    QCOMPARE( tz.first, QLatin1String( "Europe" ) );
+    QCOMPARE( tz.second, QLatin1String( "Amsterdam" ) );
+#endif
+}
+
+void
+GeoIPTests::testXML2()
+{
+    static const char data[] =
+        "<Response><TimeZone>America/North Dakota/Beulah</TimeZone></Response>";
+
+#ifdef HAVE_XML
+    XMLGeoIP handler;
+    auto tz = handler.processReply( data );
+
+    QCOMPARE( tz.first, QLatin1String( "America" ) );
+    QCOMPARE( tz.second, QLatin1String( "North Dakota/Beulah" ) );
+#endif
+}
