2da09f7648348ed3104c1c76b4d45e1fe4d7d808
Adriaan de Groot <groot@kde.org>
2018-05-10 05:37:18 -0400

    [libcalamaresui] Fix build
     - Include all headers for types that need to be fully-defined
       (e.g. return types). This guards against uses in contexts where
       those headers have not been implicitly or previously included.
    
    FIXES #948

--- src/libcalamaresui/viewpages/PythonQtGlobalStorageWrapper.h
+++ src/libcalamaresui/viewpages/PythonQtGlobalStorageWrapper.h
@@ -1,6 +1,7 @@
 /* === This file is part of Calamares - <https://github.com/calamares> ===
  *
  *   Copyright 2016, Teo Mrnjavac <teo@kde.org>
+ *   Copyright 2018, Adriaan de Groot <groot@kde.org>
  *
  *   Calamares is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -19,8 +20,9 @@
 #ifndef PYTHONQTGLOBALSTORAGEWRAPPER_H
 #define PYTHONQTGLOBALSTORAGEWRAPPER_H
 
-
 #include <QObject>
+#include <QStringList>
+#include <QVariant>
 
 namespace Calamares
 {
