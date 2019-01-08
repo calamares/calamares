527392f0afa550fd16ba51e4897f14d5afe2a492
Adriaan de Groot <groot@kde.org>
2019-01-08 22:29:55 +0100

    [libcalamares] Update Copyright statement

--- src/libcalamares/PythonJobApi.cpp
+++ src/libcalamares/PythonJobApi.cpp
@@ -1,7 +1,7 @@
 /* === This file is part of Calamares - <https://github.com/calamares> ===
  *
  *   Copyright 2014-2016, Teo Mrnjavac <teo@kde.org>
- *   Copyright 2017-2018, Adriaan de Groot <groot@kde.org>
+ *   Copyright 2017-2019, Adriaan de Groot <groot@kde.org>
  *
  *   Calamares is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
