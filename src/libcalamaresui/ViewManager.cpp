825c92582ae00973396aa45b3469da8d9d0e9331
The feren OS Dev <ferenceo@outlook.com>
2019-04-16 17:20:39 +0100

    Update ViewManager.cpp

--- src/libcalamaresui/ViewManager.cpp
+++ src/libcalamaresui/ViewManager.cpp
@@ -1,5 +1,7 @@
 /* === This file is part of Calamares - <https://github.com/calamares> ===
  *
+ *   Copyright 2019, Dominic Hayes <ferenosdev@outlook.com>
+ *   Copyright 2019, abucodonosor
  *   Copyright 2014-2015, Teo Mrnjavac <teo@kde.org>
  *   Copyright 2017-2018, Adriaan de Groot <groot@kde.org>
  *
@@ -281,6 +283,8 @@ ViewManager::next()
         {
             m_back->setEnabled( false );
             m_next->setEnabled( false );
+            if (Calamares::Settings::instance()->dontCancel())
+                m_quit->setEnabled( false );
         }
     }
     else
