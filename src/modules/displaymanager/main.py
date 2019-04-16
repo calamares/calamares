a79dfa413526c9ae1f18b73a420e766f58c8b046
The feren OS Dev <ferenceo@outlook.com>
2019-04-16 16:58:19 +0100

    Added myself to the copyright header

--- src/modules/displaymanager/main.py
+++ src/modules/displaymanager/main.py
@@ -3,6 +3,7 @@
 #
 # === This file is part of Calamares - <https://github.com/calamares> ===
 #
+#   Copyright 2019 Dominic Hayes <ferenosdev@outlook.com>
 #   Copyright 2014-2018, Philip Müller <philm@manjaro.org>
 #   Copyright 2014-2015, Teo Mrnjavac <teo@kde.org>
 #   Copyright 2014, Kevin Kofler <kevin.kofler@chello.at>
