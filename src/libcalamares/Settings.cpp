6d6c152f70f9f37fc2b521523a2169c334de6dd1
The feren OS Dev <ferenceo@outlook.com>
2019-04-16 17:14:55 +0100

    Update Settings.cpp

--- src/libcalamares/Settings.cpp
+++ src/libcalamares/Settings.cpp
@@ -1,6 +1,7 @@
 /* === This file is part of Calamares - <https://github.com/calamares> ===
  *
  *   Copyright 2019, Dominic Hayes <ferenosdev@outlook.com>
+ *   Copyright 2019, abucodonosor
  *   Copyright 2014-2015, Teo Mrnjavac <teo@kde.org>
  *   Copyright 2017-2018, Adriaan de Groot <groot@kde.org>
  *
