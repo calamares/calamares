455505ba782973b0dd060b43ec6928a27a0bca86
The feren OS Dev <ferenceo@outlook.com>
2019-04-16 18:27:03 +0100

    Update copyright header

--- src/libcalamares/Settings.cpp
+++ src/libcalamares/Settings.cpp
@@ -1,7 +1,7 @@
 /* === This file is part of Calamares - <https://github.com/calamares> ===
  *
  *   Copyright 2019, Dominic Hayes <ferenosdev@outlook.com>
- *   Copyright 2019, abucodonosor
+ *   Copyright 2019, Gabriel Craciunescu <crazy@frugalware.org>
  *   Copyright 2014-2015, Teo Mrnjavac <teo@kde.org>
  *   Copyright 2017-2018, Adriaan de Groot <groot@kde.org>
  *
