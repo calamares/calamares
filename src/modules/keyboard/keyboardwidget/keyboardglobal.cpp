cf51eb7aab13ec79dcce3e81b2e7be112017b68c
Adriaan de Groot <groot@kde.org>
2019-04-18 13:39:14 +0200

    [keyboard] Apply source formatting
     - Move errant attribution line to the copyright headers;
       unsure of the year though.
     - Apply Calamares coding style.

--- src/modules/keyboard/keyboardwidget/keyboardglobal.cpp
+++ src/modules/keyboard/keyboardwidget/keyboardglobal.cpp
@@ -7,6 +7,8 @@
  *   by Roland Singer <roland@manjaro.org>
  *   Copyright (C) 2007 Free Software Foundation, Inc.
  *
+ *   Source by Georg Grabler <ggrabler@gmail.com>
+ *
  *   Calamares is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
  *   the Free Software Foundation, either version 3 of the License, or
@@ -36,7 +38,7 @@ static const char XKB_FILE[] = "/usr/share/X11/xkb/rules/base.lst";
 // handle individual sections.
 
 /** @brief Scans a file for a named section
- * 
+ *
  * Reads from @p fh incrementally until it finds a section named @p name
  * or hits end-of-file. Returns true if the section is found. The
  * @p name must include the "! " section marker as well.
@@ -52,22 +54,23 @@ static bool findSection( QFile& fh, const char* name )
     return false;
 }
 
-//### Source by Georg Grabler <ggrabler@gmail.com> ###//
-static KeyboardGlobal::ModelsMap parseKeyboardModels(const char* filepath)
+static KeyboardGlobal::ModelsMap parseKeyboardModels( const char* filepath )
 {
     KeyboardGlobal::ModelsMap models;
 
-    QFile fh(filepath);
-    fh.open(QIODevice::ReadOnly);
+    QFile fh( filepath );
+    fh.open( QIODevice::ReadOnly );
 
-    if (!fh.isOpen()) {
+    if ( !fh.isOpen() )
+    {
         cDebug() << "X11 Keyboard model definitions not found!";
         return models;
     }
 
     bool modelsFound = findSection( fh, "! model" );
     // read the file until the end or until we break the loop
-    while (modelsFound && !fh.atEnd()) {
+    while ( modelsFound && !fh.atEnd() )
+    {
         QByteArray line = fh.readLine();
 
         // check if we start a new section
@@ -76,17 +79,18 @@ static KeyboardGlobal::ModelsMap parseKeyboardModels(const char* filepath)
 
         // here we are in the model section, otherwhise we would continue or break
         QRegExp rx;
-        rx.setPattern("^\\s+(\\S+)\\s+(\\w.*)\n$");
+        rx.setPattern( "^\\s+(\\S+)\\s+(\\w.*)\n$" );
 
         // insert into the model map
-        if (rx.indexIn(line) != -1) {
-            QString modelDesc = rx.cap(2);
-            QString model = rx.cap(1);
+        if ( rx.indexIn( line ) != -1 )
+        {
+            QString modelDesc = rx.cap( 2 );
+            QString model = rx.cap( 1 );
 
-            if (model == "pc105")
-                modelDesc += "  -  " + QObject::tr("Default Keyboard Model");
+            if ( model == "pc105" )
+                modelDesc += "  -  " + QObject::tr( "Default Keyboard Model" );
 
-            models.insert(modelDesc, model);
+            models.insert( modelDesc, model );
         }
     }
 
@@ -94,37 +98,40 @@ static KeyboardGlobal::ModelsMap parseKeyboardModels(const char* filepath)
 }
 
 
-KeyboardGlobal::LayoutsMap parseKeyboardLayouts(const char* filepath)
+KeyboardGlobal::LayoutsMap parseKeyboardLayouts( const char* filepath )
 {
     KeyboardGlobal::LayoutsMap layouts;
 
     //### Get Layouts ###//
 
-    QFile fh(filepath);
-    fh.open(QIODevice::ReadOnly);
+    QFile fh( filepath );
+    fh.open( QIODevice::ReadOnly );
 
-    if (!fh.isOpen()) {
+    if ( !fh.isOpen() )
+    {
         cDebug() << "X11 Keyboard layout definitions not found!";
         return layouts;
     }
 
     bool layoutsFound = findSection( fh, "! layout" );
     // read the file until the end or we break the loop
-    while ( layoutsFound && !fh.atEnd() ) {
+    while ( layoutsFound && !fh.atEnd() )
+    {
         QByteArray line = fh.readLine();
 
         if ( line.startsWith( '!' ) )
             break;
 
         QRegExp rx;
-        rx.setPattern("^\\s+(\\S+)\\s+(\\w.*)\n$");
+        rx.setPattern( "^\\s+(\\S+)\\s+(\\w.*)\n$" );
 
         // insert into the layout map
-        if (rx.indexIn(line) != -1) {
+        if ( rx.indexIn( line ) != -1 )
+        {
             KeyboardGlobal::KeyboardInfo info;
-            info.description = rx.cap(2);
-            info.variants.insert(QObject::tr("Default"), "");
-            layouts.insert(rx.cap(1), info);
+            info.description = rx.cap( 2 );
+            info.variants.insert( QObject::tr( "Default" ), "" );
+            layouts.insert( rx.cap( 1 ), info );
         }
     }
 
@@ -135,27 +142,32 @@ KeyboardGlobal::LayoutsMap parseKeyboardLayouts(const char* filepath)
 
     bool variantsFound = findSection( fh, "! variant" );
     // read the file until the end or until we break
-    while (variantsFound && !fh.atEnd() ) {
+    while ( variantsFound && !fh.atEnd() )
+    {
         QByteArray line = fh.readLine();
 
         if ( line.startsWith( '!' ) )
             break;
 
         QRegExp rx;
-        rx.setPattern("^\\s+(\\S+)\\s+(\\S+): (\\w.*)\n$");
+        rx.setPattern( "^\\s+(\\S+)\\s+(\\S+): (\\w.*)\n$" );
 
         // insert into the variants multimap, if the pattern matches
-        if (rx.indexIn(line) != -1) {
-            if (layouts.find(rx.cap(2)) != layouts.end()) {
+        if ( rx.indexIn( line ) != -1 )
+        {
+            if ( layouts.find( rx.cap( 2 ) ) != layouts.end() )
+            {
                 // in this case we found an entry in the multimap, and add the values to the multimap
-                layouts.find(rx.cap(2)).value().variants.insert(rx.cap(3), rx.cap(1));
-            } else {
+                layouts.find( rx.cap( 2 ) ).value().variants.insert( rx.cap( 3 ), rx.cap( 1 ) );
+            }
+            else
+            {
                 // create a new map in the multimap - the value was not found.
                 KeyboardGlobal::KeyboardInfo info;
-                info.description = rx.cap(2);
-                info.variants.insert(QObject::tr("Default"), "");
-                info.variants.insert(rx.cap(3), rx.cap(1));
-                layouts.insert(rx.cap(2), info);
+                info.description = rx.cap( 2 );
+                info.variants.insert( QObject::tr( "Default" ), "" );
+                info.variants.insert( rx.cap( 3 ), rx.cap( 1 ) );
+                layouts.insert( rx.cap( 2 ), info );
             }
         }
     }
@@ -164,12 +176,14 @@ KeyboardGlobal::LayoutsMap parseKeyboardLayouts(const char* filepath)
 }
 
 
-KeyboardGlobal::LayoutsMap KeyboardGlobal::getKeyboardLayouts() {
+KeyboardGlobal::LayoutsMap KeyboardGlobal::getKeyboardLayouts()
+{
     return parseKeyboardLayouts( XKB_FILE );
 }
 
 
-KeyboardGlobal::ModelsMap KeyboardGlobal::getKeyboardModels() {
+KeyboardGlobal::ModelsMap KeyboardGlobal::getKeyboardModels()
+{
     return parseKeyboardModels( XKB_FILE );
 }
 
