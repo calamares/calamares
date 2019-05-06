e281a7455294300ab9445aefb84597cf23cc70e2
Adriaan de Groot <groot@kde.org>
2019-05-06 10:04:28 +0200

    [finished] Fix up include guards and include style

--- src/modules/finished/FinishedViewStep.h
+++ src/modules/finished/FinishedViewStep.h
@@ -1,6 +1,7 @@
 /* === This file is part of Calamares - <https://github.com/calamares> ===
  *
  *   Copyright 2014-2015, Teo Mrnjavac <teo@kde.org>
+ *   Copyright 2019, Adriaan de Groot <groot@kde.org>
  *
  *   Calamares is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -16,15 +17,15 @@
  *   along with Calamares. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef FINISHEDPAGEPLUGIN_H
-#define FINISHEDPAGEPLUGIN_H
+#ifndef FINISHEDVIEWSTEP_H
+#define FINISHEDVIEWSTEP_H
 
 #include <QObject>
 
-#include <utils/PluginFactory.h>
-#include <viewpages/ViewStep.h>
+#include "utils/PluginFactory.h"
+#include "viewpages/ViewStep.h"
 
-#include <PluginDllMacro.h>
+#include "PluginDllMacro.h"
 
 class FinishedPage;
 
@@ -70,4 +71,4 @@ private:
 
 CALAMARES_PLUGIN_FACTORY_DECLARATION( FinishedViewStepFactory )
 
-#endif // FINISHEDPAGEPLUGIN_H
+#endif
