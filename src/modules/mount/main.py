ecc32ab9b68012ea9072ee9dcca627aeb4ddd642
Kevin Kofler <kevin.kofler@chello.at>
2019-05-09 13:52:13 +0200

    [mount] Update copyright notice

--- src/modules/mount/main.py
+++ src/modules/mount/main.py
@@ -6,6 +6,7 @@
 #   Copyright 2014, Aurélien Gâteau <agateau@kde.org>
 #   Copyright 2017, Alf Gaida <agaida@siduction.org>
 #   Copyright 2019, Adriaan de Groot <groot@kde.org>
+#   Copyright 2019, Kevin Kofler <kevin.kofler@chello.at>
 #
 #   Calamares is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
