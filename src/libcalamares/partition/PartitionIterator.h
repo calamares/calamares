d8dc512f45069f2d07c36710efd7c4ab7c13782a
Adriaan de Groot <groot@kde.org>
2019-06-13 12:12:47 +0200

    [libcalamares] Move PartitionIterator to partition service.
     - Starting to centralize utility code for partitioning into
       libcalamares instead of scattered and weirdly shared between
       modules.
     - This particular commit breaks compiling the modules, though.

--- src/libcalamares/partition/PartitionIterator.h
+++ src/libcalamares/partition/PartitionIterator.h
@@ -2,6 +2,7 @@
  *
  *   Copyright 2014, Aurélien Gâteau <agateau@kde.org>
  *   Copyright 2015, Teo Mrnjavac <teo@kde.org>
+ *   Copyright 2019, Adriaan de Groot <groot@kde.org>
  *
  *   Calamares is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -17,20 +18,34 @@
  *   along with Calamares. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef PARTITIONITERATOR_H
-#define PARTITIONITERATOR_H
+#ifndef PARTITION_PARTITIONITERATOR_H
+#define PARTITION_PARTITIONITERATOR_H
 
 class Device;
 class Partition;
 class PartitionTable;
 
-/**
+namespace CalamaresUtils
+{
+namespace Partition
+{
+
+/** @brief Iterator over KPMCore partitions
+ *
  * A forward-only iterator to go through the partitions of a device,
  * independently of whether they are primary, logical or extended.
+ *
+ * An iterator can be created from a device (then it refers to the
+ * partition table of that device) or a partition table. The
+ * partition table must remain valid throughout iteration.
+ *
+ * A nullptr is valid, for an empty iterator.
  */
 class PartitionIterator
 {
 public:
+    using Partition = ::Partition;
+
     Partition* operator*() const;
 
     void operator++();
@@ -50,4 +65,7 @@ private:
     Partition* m_current = nullptr;
 };
 
-#endif /* PARTITIONITERATOR_H */
+}  // namespace Partition
+}  // namespace CalamaresUtils
+
+#endif  // PARTITION_PARTITIONITERATOR_H
