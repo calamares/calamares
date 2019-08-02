d76c33bd3aa7e7b7fa59118a590f3ccd1bc1213e
Adriaan de Groot <groot@kde.org>
2019-08-02 09:19:15 +0200

    [tracking] Tidy code some more

--- src/modules/tracking/TrackingJobs.cpp
+++ src/modules/tracking/TrackingJobs.cpp
@@ -124,24 +124,32 @@ TrackingMachineNeonJob::prettyStatusMessage() const
 Calamares::JobResult
 TrackingMachineNeonJob::exec()
 {
-    int r = CalamaresUtils::System::instance()->targetEnvCall( "/bin/sh",
-                                                               QString(),  // Working dir
-                                                               QString(
-                                                                   R"x(MACHINE_ID=`cat /etc/machine-id`
+    static const auto script = QStringLiteral(
+        R"x(
+MACHINE_ID=`cat /etc/machine-id`
 sed -i "s,URI =.*,URI = http://releases.neon.kde.org/meta-release/${MACHINE_ID}," /etc/update-manager/meta-release
 sed -i "s,URI_LTS =.*,URI_LTS = http://releases.neon.kde.org/meta-release-lts/${MACHINE_ID}," /etc/update-manager/meta-release
 true
-)x" ),
+)x" );
+    int r = CalamaresUtils::System::instance()->targetEnvCall( "/bin/sh",
+                                                               QString(),  // Working dir
+                                                               script,
                                                                std::chrono::seconds( 1 ) );
 
     if ( r == 0 )
+    {
         return Calamares::JobResult::ok();
+    }
     else if ( r > 0 )
+    {
         return Calamares::JobResult::error(
             tr( "Error in machine feedback configuration." ),
             tr( "Could not configure machine feedback correctly, script error %1." ).arg( r ) );
+    }
     else
+    {
         return Calamares::JobResult::error(
             tr( "Error in machine feedback configuration." ),
             tr( "Could not configure machine feedback correctly, Calamares error %1." ).arg( r ) );
+    }
 }
