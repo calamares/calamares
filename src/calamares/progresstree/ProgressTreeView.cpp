27c3a5a11d4739e5fe7081e72b48ed5318443f42
Adriaan de Groot <groot@kde.org>
2019-08-13 20:51:46 +0200

    [calamares] Apply coding style

--- src/calamares/progresstree/ProgressTreeView.cpp
+++ src/calamares/progresstree/ProgressTreeView.cpp
@@ -76,10 +76,9 @@ ProgressTreeView::setModel( QAbstractItemModel* model )
     QTreeView::setModel( model );
     expandAll();
 
-    connect(
-        Calamares::ViewManager::instance(),
-        &Calamares::ViewManager::currentStepChanged,
-        this,
-        [this]() { viewport()->update(); },
-        Qt::UniqueConnection );
+    connect( Calamares::ViewManager::instance(),
+             &Calamares::ViewManager::currentStepChanged,
+             this,
+             [this]() { viewport()->update(); },
+             Qt::UniqueConnection );
 }
