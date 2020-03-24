184462a8758cb5eb51510a9960f90cde1451518b
Adriaan de Groot <groot@kde.org>
2020-03-24 15:14:38 +0100

    [libcalamares] Make status of JobQueue queryable
    - while the queue is executing (the thread is running jobs) the
      isRunning() method returns true.
    - re-work some internals to reset isRunning() before emitting
      finished() signal.

--- src/libcalamares/JobQueue.cpp
+++ src/libcalamares/JobQueue.cpp
@@ -140,7 +140,7 @@ private:
             m_queue, "failed", Qt::QueuedConnection, Q_ARG( QString, message ), Q_ARG( QString, details ) );
     }
 
-    void emitFinished() { QMetaObject::invokeMethod( m_queue, "finished", Qt::QueuedConnection ); }
+    void emitFinished() { QMetaObject::invokeMethod( m_queue, "finish", Qt::QueuedConnection ); }
 };
 
 JobThread::~JobThread() {}
@@ -195,6 +195,7 @@ JobQueue::start()
     Q_ASSERT( !m_thread->isRunning() );
     m_thread->setJobs( std::move( m_jobs ) );
     m_jobs.clear();
+    m_finished = false;
     m_thread->start();
 }
 
@@ -214,6 +215,13 @@ JobQueue::enqueue( const JobList& jobs )
     Q_ASSERT( !m_thread->isRunning() );
     m_jobs.append( jobs );
     emit queueChanged( m_jobs );
+}
+
+void
+JobQueue::finish()
+{
+    m_finished = true;
+    emit finished();
 }
 
 }  // namespace Calamares
