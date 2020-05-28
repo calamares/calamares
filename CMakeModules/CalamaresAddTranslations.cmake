0d4642ca6a94f05ea279b5cdbb738a483c5baa2e
Callum Farmer <callumjfarmer13@gmail.com>
2020-05-28 21:46:47 +0100

    [CMakeModules] Update SPDX identifiers.
    Update CalamaresAddTest.cmake
    
    Update CalamaresAddLibrary.cmake
    
    Update CalamaresAddBrandingSubdirectory.cmake
    
    Update CalamaresAddTranslations.cmake
    
    Update CalamaresAddPlugin.cmake
    
    Update CalamaresAddModuleSubdirectory.cmake

--- CMakeModules/CalamaresAddTranslations.cmake
+++ CMakeModules/CalamaresAddTranslations.cmake
@@ -13,7 +13,7 @@
 #   You should have received a copy of the GNU General Public License
 #   along with Calamares. If not, see <http://www.gnu.org/licenses/>.
 #
-#   SPDX-License-Identifier: GPL-3.0+
+#   SPDX-License-Identifier: GPL-3.0-or-later
 #   License-Filename: LICENSE
 #
 ###
