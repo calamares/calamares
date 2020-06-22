e24f812b2d29e2833668b2c4a553a9d9ef1e496b
Adriaan de Groot <groot@kde.org>
2020-06-22 16:32:47 -0400

    [libcalamares] Chase Permissions move
    - Fix include names in *preservefiles*
    - Tidy up include guards
    - Fix CMakeLists in *perservefiles* and *libcalamares*
    - Use SPDX license headers

--- src/libcalamares/utils/Permissions.h
+++ src/libcalamares/utils/Permissions.h
@@ -1,45 +1,35 @@
 /* === This file is part of Calamares - <https://github.com/calamares> ===
  *
- *  Copyright (C) 2018 Scott Harvey <scott@spharvey.me>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
-
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *  SPDX-FileCopyrightText: 2018 Scott Harvey <scott@spharvey.me>
+ *  SPDX-License-Identifier: GPL-3.0-or-later
+ *  License-Filename: LICENSE
  *
  */
 
-#ifndef PERMISSIONS_H
-#define PERMISSIONS_H
+#ifndef LIBCALAMARES_PERMISSIONS_H
+#define LIBCALAMARES_PERMISSIONS_H
+
+#include "DllMacro.h"
 
 #include <QString>
 
 /**
- * @brief The Permissions class takes a QString @p in the form of 
+ * @brief The Permissions class takes a QString @p in the form of
  * <user>:<group>:<permissions>, checks it for validity, and makes the three
  * components available indivdually.
  */
-class Permissions
+class DLLEXPORT Permissions
 {
 
 public:
-
     /** @brief Constructor
-     * 
-     * Splits the string @p at the colon (":") into separate elements for 
+     *
+     * Splits the string @p at the colon (":") into separate elements for
      * <user>, <group>, and <value> (permissions), where <value> is returned as
      * an **octal** integer.
      */
-    Permissions(QString p);
-    
+    Permissions( QString p );
+
     /** @brief Default constructor of an invalid Permissions. */
     Permissions();
 
@@ -50,13 +40,12 @@ public:
     QString octal() const { return QString::number( m_value, 8 ); }
 
 private:
-    void parsePermissions(QString const &p);
+    void parsePermissions( QString const& p );
 
     QString m_username;
     QString m_group;
     bool m_valid;
     int m_value;
-
 };
 
-#endif // PERMISSIONS_H
+#endif  // LIBCALAMARES_PERMISSIONS_H
