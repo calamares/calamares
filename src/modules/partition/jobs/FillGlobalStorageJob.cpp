46ad704ede0fa6aff65342814f46ec5e25619826
Adriaan de Groot <groot@kde.org>
2020-07-03 22:33:00 +0200

    [partition] Fix build for old KPMCore
    SEE #1444

--- src/modules/partition/jobs/FillGlobalStorageJob.cpp
+++ src/modules/partition/jobs/FillGlobalStorageJob.cpp
@@ -93,8 +93,10 @@ mapForPartition( Partition* partition, const QString& uuid )
     map[ "device" ] = partition->partitionPath();
     map[ "partlabel" ] = partition->label();
     map[ "partuuid" ] = partition->uuid();
+#ifdef WITH_KPMCORE42API
     map[ "parttype" ] = partition->type();
     map[ "partattrs" ] = partition->attributes();
+#endif
     map[ "mountPoint" ] = PartitionInfo::mountPoint( partition );
     map[ "fsName" ] = userVisibleFS( partition->fileSystem() );
     map[ "fs" ] = untranslatedFS( partition->fileSystem() );
