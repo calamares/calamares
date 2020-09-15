8be5c2ed10a2ed4221eafc530737f52ecfc80aab
Adriaan de Groot <groot@kde.org>
2020-09-15 12:57:09 +0200

    [machineid] Support multiple entropy files

--- src/modules/machineid/MachineIdJob.h
+++ src/modules/machineid/MachineIdJob.h
@@ -11,6 +11,7 @@
 #define MACHINEIDJOB_H
 
 #include <QObject>
+#include <QStringList>
 #include <QVariantMap>
 
 #include "CppJob.h"
@@ -19,6 +20,9 @@
 
 #include "DllMacro.h"
 
+/** @brief Write 'random' data: machine id, entropy, UUIDs
+ *
+ */
 class PLUGINDLLEXPORT MachineIdJob : public Calamares::CppJob
 {
     Q_OBJECT
@@ -33,14 +37,22 @@ public:
 
     void setConfigurationMap( const QVariantMap& configurationMap ) override;
 
+    /** @brief The list of filenames to write full of entropy.
+     *
+     * The list may be empty (no entropy files are configure) or
+     * contain one or more filenames to be interpreted within the
+     * target system.
+     */
+    QStringList entropyFileNames() const { return m_entropy_files; }
+
 private:
     bool m_systemd = false;  ///< write systemd's files
 
     bool m_dbus = false;  ///< write dbus files
     bool m_dbus_symlink = false;  ///< .. or just symlink to systemd
 
-    bool m_entropy = false;  ///< write an entropy file
     bool m_entropy_copy = false;  ///< copy from host system
+    QStringList m_entropy_files;  ///< names of files to write
 };
 
 CALAMARES_PLUGIN_FACTORY_DECLARATION( MachineIdJobFactory )
