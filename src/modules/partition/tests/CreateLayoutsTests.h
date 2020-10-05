a7bd1040c5d64ab134bbd58e8296a16ebddbdedb
Corentin Noël <corentin.noel@collabora.com>
2020-10-05 11:40:04 +0200

    [partition] Add tests for Layout-constrained partionning

---
+++ src/modules/partition/tests/CreateLayoutsTests.h
@@ -0,0 +1,36 @@
+/* === This file is part of Calamares - <https://calamares.io> ===
+ *
+ *   SPDX-FileCopyrightText: 2020 Corentin Noël <corentin.noel@collabora.com>
+ *   SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ *   Calamares is Free Software: see the License-Identifier above.
+ *
+ */
+
+#ifndef CLEARMOUNTSJOBTESTS_H
+#define CLEARMOUNTSJOBTESTS_H
+
+#include <QObject>
+#include <core/device.h>
+
+class CreateLayoutsTests : public QObject
+{
+    Q_OBJECT
+public:
+    CreateLayoutsTests();
+
+private Q_SLOTS:
+    void testFixedSizePartition();
+    void testPercentSizePartition();
+    void testMixedSizePartition();
+    void init();
+    void cleanup();
+};
+
+class TestDevice : public Device
+{
+public:
+    TestDevice(const QString& name, const qint64 logicalSectorSize, const qint64 totalLogicalSectors);
+};
+
+#endif
