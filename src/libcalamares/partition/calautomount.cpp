c39e8802dd0ced25269ae9aceb3088d7ccdcd284
Adriaan de Groot <groot@kde.org>
2020-12-22 22:07:17 +0100

    [libcalamares] coding-style, logging in calautomount

--- src/libcalamares/partition/calautomount.cpp
+++ src/libcalamares/partition/calautomount.cpp
@@ -18,31 +18,35 @@
  */
 
 static const char usage[] = "Usage: calautomount <-e|-d>\n"
-"\n"
-"Enables (if `-e` is passed as command-line option) or\n"
-"Disables (if `-d` is passed as command-line option)\n"
-"\n"
-"automounting of disks in the host system as best it can.\n"
-"Exits with code 0 on success or 1 if an unknown option is\n"
-"passed on the command-line.\n\n";
+                            "\n"
+                            "Enables (if `-e` is passed as command-line option) or\n"
+                            "Disables (if `-d` is passed as command-line option)\n"
+                            "\n"
+                            "automounting of disks in the host system as best it can.\n"
+                            "Exits with code 0 on success or 1 if an unknown option is\n"
+                            "passed on the command-line.\n\n";
 
 #include "AutoMount.h"
 #include "Sync.h"
+#include "utils/Logger.h"
 
 #include <QCoreApplication>
 #include <QDebug>
 
-int main(int argc, char **argv)
+int
+main( int argc, char** argv )
 {
-    QCoreApplication app(argc, argv);
+    QCoreApplication app( argc, argv );
 
-    if ((argc != 2) || (argv[1][0] != '-') || (argv[1][1] !='e' && argv[1][1] !='d'))
+    if ( ( argc != 2 ) || ( argv[ 1 ][ 0 ] != '-' ) || ( argv[ 1 ][ 1 ] != 'e' && argv[ 1 ][ 1 ] != 'd' ) )
     {
         qWarning() << usage;
         return 1;
     }
 
-    CalamaresUtils::Partition::automountDisable( argv[1][1] == 'd');
+    Logger::setupLogfile();
+    Logger::setupLogLevel( Logger::LOGDEBUG );
+    CalamaresUtils::Partition::automountDisable( argv[ 1 ][ 1 ] == 'd' );
 
     return 0;
 }
