c98a330bf96449baf45ce206df8e45924e4c3d0c
Adriaan de Groot <groot@kde.org>
2021-02-03 00:46:00 +0100

    [libcalamares] Store DBus reply value, drop debug-logging

--- src/libcalamares/partition/AutoMount.cpp
+++ src/libcalamares/partition/AutoMount.cpp
@@ -12,6 +12,8 @@
 
 #include <QtDBus>
 
+#include <optional>
+
 namespace CalamaresUtils
 {
 namespace Partition
@@ -58,13 +60,21 @@ querySolidAutoMount( QDBusConnection& dbus, AutoMountInfo& info )
     // Find previous setting; this **does** need to block
     auto msg = kdedCall( QStringLiteral( "isModuleAutoloaded" ) );
     msg.setArguments( { moduleName } );
+    std::optional< bool > result;
     QDBusMessage r = dbus.call( msg, QDBus::Block );
     if ( r.type() == QDBusMessage::ReplyMessage )
     {
         auto arg = r.arguments();
-        cDebug() << arg;
-        info.wasSolidModuleAutoLoaded = false;
+        if ( arg.length() == 1 )
+        {
+            auto v = arg.at( 0 );
+            if ( v.isValid() && v.type() == QVariant::Bool )
+            {
+                result = v.toBool();
+            }
+        }
     }
+    info.wasSolidModuleAutoLoaded = result.has_value() ? result.value() : false;
 }
 
 std::shared_ptr< AutoMountInfo >
