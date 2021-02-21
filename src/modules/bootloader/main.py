e0f40cb4a4b7691ebb54092adb2ccced6c2f59a7
dalto <dalto@fastmail.com>
2021-02-21 17:36:11 -0600

    Remove EOS-specific changes

--- src/modules/bootloader/main.py
+++ src/modules/bootloader/main.py
@@ -322,20 +322,6 @@ def install_systemd_boot(efi_directory):
 
     create_loader(loader_path, distribution_translated)
 
-    # clean up redundant kernel and initrd
-    try:
-        os.remove(os.path.join(install_path, "boot/vmlinuz-linux"))
-    except FileNotFoundError:
-        pass
-    try:
-        os.remove(os.path.join(install_path, "boot/initramfs-linux.img"))
-    except FileNotFoundError:
-        pass
-    try:
-        os.remove(os.path.join(install_path, "boot/initramfs-linux-fallback.img"))
-    except FileNotFoundError:
-        pass
-
 
 def get_grub_efi_parameters():
     """
