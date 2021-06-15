629fc83f210367b858c0f93a5ca220fadaab1973
Adriaan de Groot <groot@kde.org>
2021-06-15 21:50:59 +0200

    [libcalamares] Add a pre-script for PythonJobs
    This allows injecting arbitrary Python code before
    the script of a module is even run. For testing
    purposes, that gives us a chance to modify existing
    (internal) modules before the script (e.g. to test
    subprocess calls).

--- src/libcalamares/PythonJob.h
+++ src/libcalamares/PythonJob.h
@@ -41,6 +41,19 @@ public:
     QString prettyStatusMessage() const override;
     JobResult exec() override;
 
+    /** @brief Sets the pre-run Python code for all PythonJobs
+     *
+     * A PythonJob runs the code from the scriptFile parameter to
+     * the constructor; the pre-run code is **also** run, before
+     * even the scriptFile code. Use this in testing mode
+     * to modify Python internals.
+     *
+     * No ownership of @p script is taken: pass in a pointer to
+     * a character literal or something that lives longer than the
+     * job. Pass in @c nullptr to switch off pre-run code.
+     */
+    static void setInjectedPreScript( const char* script );
+
 private:
     struct Private;
 
