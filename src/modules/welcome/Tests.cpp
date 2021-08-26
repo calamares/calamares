f376b42c31e4befd9d87c55ac80130bfd165f492
Adriaan de Groot <groot@kde.org>
2021-08-26 16:58:46 +0200

    [welcome] Add a handful of tests for different URL configs

--- src/modules/welcome/Tests.cpp
+++ src/modules/welcome/Tests.cpp
@@ -29,6 +29,8 @@ private Q_SLOTS:
     void initTestCase();
 
     void testOneUrl();
+    void testUrls_data();
+    void testUrls();
 };
 
 WelcomeTests::WelcomeTests() {}
@@ -71,6 +73,41 @@ WelcomeTests::testOneUrl()
     QCOMPARE( CalamaresUtils::Network::Manager::instance().getCheckInternetUrls().count(), 1 );
 }
 
+void
+WelcomeTests::testUrls_data()
+{
+    QTest::addColumn< QString >( "filename" );
+    QTest::addColumn< int >( "result" );
+
+    QTest::newRow( "one  " ) << QString( "1a-checkinternet.conf" ) << 1;
+    QTest::newRow( "none " ) << QString( "1b-checkinternet.conf" ) << 0;
+    QTest::newRow( "blank" ) << QString( "1c-checkinternet.conf" ) << 0;
+    QTest::newRow( "bogus" ) << QString( "1d-checkinternet.conf" ) << 0;
+    QTest::newRow( "[]   " ) << QString( "1e-checkinternet.conf" ) << 0;
+    QTest::newRow( "-3   " ) << QString( "1f-checkinternet.conf" ) << 3;
+    QTest::newRow( "[3]  " ) << QString( "1g-checkinternet.conf" ) << 3;
+    QTest::newRow( "some " ) << QString( "1h-checkinternet.conf" ) << 3;
+}
+
+void
+WelcomeTests::testUrls()
+{
+    QFETCH( QString, filename );
+    QFETCH( int, result );
+
+    Config c;
+
+    // BUILD_AS_TEST is the source-directory path
+    QFile fi( QString( "%1/tests/%2" ).arg( BUILD_AS_TEST, filename ) );
+    QVERIFY( fi.exists() );
+
+    bool ok = false;
+    const auto map = CalamaresUtils::loadYaml( fi, &ok );
+    QVERIFY( ok );
+
+    c.setConfigurationMap( map );
+    QCOMPARE( CalamaresUtils::Network::Manager::instance().getCheckInternetUrls().count(), result );
+}
 
 QTEST_GUILESS_MAIN( WelcomeTests )
 
