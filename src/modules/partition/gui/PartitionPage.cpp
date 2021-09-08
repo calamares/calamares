226419f7947a8d0f86b6de58e6ec389eeb9766ca
Adriaan de Groot <groot@kde.org>
2021-09-08 10:58:38 +0200

    [partition] Translate button texts in warning boxes

--- src/modules/partition/gui/PartitionPage.cpp
+++ src/modules/partition/gui/PartitionPage.cpp
@@ -258,13 +258,16 @@ PartitionPage::checkCanCreate( Device* device )
 
         if ( ( table->numPrimaries() >= table->maxPrimaries() ) && !table->hasExtended() )
         {
-            QMessageBox::warning(
-                this,
+            QMessageBox mb(
+                QMessageBox::Warning,
                 tr( "Can not create new partition" ),
                 tr( "The partition table on %1 already has %2 primary partitions, and no more can be added. "
                     "Please remove one primary partition and add an extended partition, instead." )
                     .arg( device->name() )
-                    .arg( table->numPrimaries() ) );
+                    .arg( table->numPrimaries() ),
+                QMessageBox::Ok );
+            Calamares::fixButtonLabels( &mb );
+            mb.exec();
             return false;
         }
         return true;
