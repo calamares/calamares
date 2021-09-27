f197acf8e00d20db25f267604685898dc0547daa
Adriaan de Groot <groot@kde.org>
2021-09-27 17:27:42 +0200

    CI: add stubs for libcalamares bits, to help pylint

---
+++ ci/libcalamares/utils.py
@@ -0,0 +1,15 @@
+#   SPDX-FileCopyrightText: no
+#   SPDX-License-Identifier: CC0-1.0
+#
+# Stubs for part of the Python API from libcalamares
+# (although the **actual** API is presented through
+# Boost::Python, not as a bare C-extension) so that
+# pylint doesn't complain about libcalamares internals.
+
+def debug(_): pass
+
+def warning(_): pass
+
+def gettext_path(): pass
+
+def gettext_languages(): pass
