1260d3fcb9e67c6d59707fc21d8ebb01fb4bc820
Adriaan de Groot <groot@kde.org>
2021-11-29 13:21:50 +0100

    [packages] Expand tests for PM-specifics more

--- src/modules/packages/tests/test-pm-pacman.py
+++ src/modules/packages/tests/test-pm-pacman.py
@@ -11,12 +11,26 @@ from src.modules.packages import main
 
 # .. we don't have a job in this test, so fake one
 class Job(object):
-    def __init__(self):
-        self.configuration = libcalamares.utils.load_yaml("pm-pacman.yaml")
-libcalamares.job = Job()
+    def __init__(self, filename):
+        self.configuration = libcalamares.utils.load_yaml(filename) if filename is not None else dict()
+
+import sys
+if len(sys.argv) > 4:
+    filename = sys.argv[1]
+    retry = int(sys.argv[2])
+    timeout = bool(int(sys.argv[3]))
+    needed = bool(int(sys.argv[4]))
+else:
+    filename = None
+    retry = 0
+    timeout = False
+    needed = False
+
+libcalamares.utils.warning("Expecting {!s} retry={!s} timeout={!s} needed={!s}".format(filename, retry, timeout, needed))
 
 # Specific PM test
+libcalamares.job = Job(filename)
 p = main.PMPacman()
-assert p.pacman_num_retries == 0
-assert p.pacman_disable_timeout == False
-assert p.pacman_needed_only == False
+assert p.pacman_num_retries == retry, "{!r} vs {!r}".format(p.pacman_num_retries, retry)
+assert p.pacman_disable_timeout == timeout
+assert p.pacman_needed_only == needed
