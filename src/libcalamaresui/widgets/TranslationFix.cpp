aa332477fd1858603f528a5f036925624446249a
Artem Grinev <agrinev@manjaro.org>
2021-12-06 03:11:16 +0400

    [libcalamaresui] Run clang-format on TranslationFix.cpp

--- src/libcalamaresui/widgets/TranslationFix.cpp
+++ src/libcalamaresui/widgets/TranslationFix.cpp
@@ -10,34 +10,35 @@
 #include "TranslationFix.h"
 
 #include <QAbstractButton>
-#include <QPushButton>
 #include <QCoreApplication>
-#include <QMessageBox>
 #include <QDialogButtonBox>
+#include <QMessageBox>
+#include <QPushButton>
 
 namespace Calamares
 {
 
 //Using QMessageBox's StandardButton enum here but according to headers they should be kept in-sync between multiple classes.
 static std::pair< decltype( QMessageBox::Ok ), const char* > maps[] = {
-        { QMessageBox::Ok, QT_TRANSLATE_NOOP( "StandardButtons", "&OK" ) },
-        { QMessageBox::Yes, QT_TRANSLATE_NOOP( "StandardButtons", "&Yes" ) },
-        { QMessageBox::No, QT_TRANSLATE_NOOP( "StandardButtons", "&No" ) },
-        { QMessageBox::Cancel, QT_TRANSLATE_NOOP( "StandardButtons", "&Cancel" ) },
-        { QMessageBox::Close, QT_TRANSLATE_NOOP( "StandardButtons", "&Close" ) },
-    };
+    { QMessageBox::Ok, QT_TRANSLATE_NOOP( "StandardButtons", "&OK" ) },
+    { QMessageBox::Yes, QT_TRANSLATE_NOOP( "StandardButtons", "&Yes" ) },
+    { QMessageBox::No, QT_TRANSLATE_NOOP( "StandardButtons", "&No" ) },
+    { QMessageBox::Cancel, QT_TRANSLATE_NOOP( "StandardButtons", "&Cancel" ) },
+    { QMessageBox::Close, QT_TRANSLATE_NOOP( "StandardButtons", "&Close" ) },
+};
 
-template<typename TButtonBox>
-void fixButtonLabels ( TButtonBox* box )
+template < typename TButtonBox >
+void
+fixButtonLabels( TButtonBox* box )
 {
     if ( !box )
     {
         return;
     }
-    
+
     for ( auto [ sb, label ] : maps )
     {
-        auto* button = box->button( static_cast<typename TButtonBox::StandardButton>(int(sb)) );
+        auto* button = box->button( static_cast< typename TButtonBox::StandardButton >( int( sb ) ) );
         if ( button )
         {
             button->setText( QCoreApplication::translate( "StandardButtons", label ) );
@@ -48,13 +49,13 @@ void fixButtonLabels ( TButtonBox* box )
 void
 fixButtonLabels( QMessageBox* box )
 {
-    fixButtonLabels<QMessageBox>(box);
+    fixButtonLabels< QMessageBox >( box );
 }
 
 void
-fixButtonLabels(QDialogButtonBox *box)
+fixButtonLabels( QDialogButtonBox* box )
 {
-    fixButtonLabels<QDialogButtonBox>(box);
+    fixButtonLabels< QDialogButtonBox >( box );
 }
 
 }  // namespace Calamares
