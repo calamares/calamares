32c5e18db0e75833e9ad599694edc0d040c9ae22
Artem Grinev <agrinev@manjaro.org>
2021-12-06 02:26:13 +0400

    [libcalamaresui] Add QDialogButtonBox translation fix

--- src/libcalamaresui/widgets/TranslationFix.cpp
+++ src/libcalamaresui/widgets/TranslationFix.cpp
@@ -10,21 +10,16 @@
 #include "TranslationFix.h"
 
 #include <QAbstractButton>
+#include <QPushButton>
 #include <QCoreApplication>
 #include <QMessageBox>
+#include <QDialogButtonBox>
 
 namespace Calamares
 {
 
-void
-fixButtonLabels( QMessageBox* box )
-{
-    if ( !box )
-    {
-        return;
-    }
-
-    static std::pair< decltype( QMessageBox::Ok ), const char* > maps[] = {
+//Using QMessageBox's StandardButton enum here but according to headers they should be kept in-sync between multiple classes.
+static std::pair< decltype( QMessageBox::Ok ), const char* > maps[] = {
         { QMessageBox::Ok, QT_TRANSLATE_NOOP( "StandardButtons", "&OK" ) },
         { QMessageBox::Yes, QT_TRANSLATE_NOOP( "StandardButtons", "&Yes" ) },
         { QMessageBox::No, QT_TRANSLATE_NOOP( "StandardButtons", "&No" ) },
@@ -32,14 +27,34 @@ fixButtonLabels( QMessageBox* box )
         { QMessageBox::Close, QT_TRANSLATE_NOOP( "StandardButtons", "&Close" ) },
     };
 
+template<typename TButtonBox>
+void fixButtonLabels ( TButtonBox* box )
+{
+    if ( !box )
+    {
+        return;
+    }
+    
     for ( auto [ sb, label ] : maps )
     {
-        auto* button = box->button( sb );
+        auto* button = box->button( static_cast<typename TButtonBox::StandardButton>(int(sb)) );
         if ( button )
         {
             button->setText( QCoreApplication::translate( "StandardButtons", label ) );
         }
     }
+}
+
+void
+fixButtonLabels( QMessageBox* box )
+{
+    fixButtonLabels<QMessageBox>(box);
+}
+
+void
+fixButtonLabels(QDialogButtonBox *box)
+{
+    fixButtonLabels<QDialogButtonBox>(box);
 }
 
 }  // namespace Calamares
