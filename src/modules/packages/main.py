b5bad4fab433d6e3225cdb09dddd6e7f8a15c4a2
Kenny Strawn <Kenny.Strawn@gmail.com>
2022-03-22 09:35:32 -0700

    Work around yay's tendency to write to PWD if no home directory present

--- src/modules/packages/main.py
+++ src/modules/packages/main.py
@@ -547,6 +547,7 @@ class PMyay(PackageManager):
     def install(self, pkgs, from_local=False):
         import os
         os.environ["XDG_CACHE_HOME"] = "/tmp"
+        os.environ["PWD"] = "/tmp"
         command = ["sudo", "-E", "-u", "nobody", "yay"]
 
         if from_local:
@@ -574,17 +575,20 @@ class PMyay(PackageManager):
     def remove(self, pkgs):
         import os
         os.environ["XDG_CACHE_HOME"] = "/tmp"
+        os.environ["PWD"] = "/tmp"
         self.reset_progress()
         self.run_yay(["sudo", "-E", "-u", "nobody", "yay", "-Rs", "--noconfirm"] + pkgs, True)
 
     def update_db(self):
         import os
         os.environ["XDG_CACHE_HOME"] = "/tmp"
+        os.environ["PWD"] = "/tmp"
         self.run_yay(["sudo", "-E", "-u", "nobody", "yay", "-Sy"])
 
     def update_system(self):
         import os
         os.environ["XDG_CACHE_HOME"] = "/tmp"
+        os.environ["PWD"] = "/tmp"
         command = ["sudo", "-E", "-u", "nobody", "yay", "-Su", "--noconfirm"]
         if self.yay_disable_timeout is True:
             command.append("--disable-download-timeout")
