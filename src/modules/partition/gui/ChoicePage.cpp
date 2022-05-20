51e3ac0ded43d4e604a963faaa85cf602da04b99
Adriaan de Groot <groot@kde.org>
2022-05-20 11:54:45 +0200

    [partition] Polish up debugging when checking for next-enabled
    - Improve the messages a bit
    - Remove useless variable (it is checking that the switch(choice)
      statement covers all the cases; let's leave that to the compiler)

--- src/modules/partition/gui/ChoicePage.cpp
+++ src/modules/partition/gui/ChoicePage.cpp
@@ -1610,34 +1610,26 @@ ChoicePage::isNextEnabled() const
 bool
 ChoicePage::calculateNextEnabled() const
 {
-    bool enabled = false;
     auto sm_p = m_beforePartitionBarsView ? m_beforePartitionBarsView->selectionModel() : nullptr;
 
     switch ( m_config->installChoice() )
     {
     case InstallChoice::NoChoice:
-        cDebug() << "No partitioning choice";
+        cDebug() << "No partitioning choice has been made yet";
         return false;
     case InstallChoice::Replace:
     case InstallChoice::Alongside:
         if ( !( sm_p && sm_p->currentIndex().isValid() ) )
         {
-            cDebug() << "No partition selected";
+            cDebug() << "No partition selected for alongside or replace";
             return false;
         }
-        enabled = true;
         break;
     case InstallChoice::Erase:
     case InstallChoice::Manual:
-        enabled = true;
+        // Nothing to check for these
+        break;
     }
-
-    if ( !enabled )
-    {
-        cDebug() << "No valid choice made";
-        return false;
-    }
-
 
     if ( m_isEfi
          && ( m_config->installChoice() == InstallChoice::Alongside
@@ -1655,7 +1647,7 @@ ChoicePage::calculateNextEnabled() const
         switch ( m_encryptWidget->state() )
         {
         case EncryptWidget::Encryption::Unconfirmed:
-            cDebug() << "No passphrase provided";
+            cDebug() << "No passphrase provided or passphrase mismatch.";
             return false;
         case EncryptWidget::Encryption::Disabled:
         case EncryptWidget::Encryption::Confirmed:
