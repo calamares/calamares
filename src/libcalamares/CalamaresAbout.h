09255a1d9c81e07427181d3a0bbce5ba73583dd6
Adriaan de Groot <groot@kde.org>
2022-05-23 16:40:39 +0200

    [libcalamares] Add a more extensible About message
    - make the years easier to update without annoying translators
    - make the maintainers extensible.

---
+++ src/libcalamares/CalamaresAbout.h
@@ -0,0 +1,23 @@
+/* === This file is part of Calamares - <https://calamares.io> ===
+ *
+ *   SPDX-FileCopyrightText: 2022 Adriaan de Groot <groot@kde.org>
+ *   SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ *   Calamares is Free Software: see the License-Identifier above.
+ *
+ */
+
+#ifndef CALAMARES_CALAMARESABOUT_H
+#define CALAMARES_CALAMARESABOUT_H
+
+#include "DllMacro.h"
+
+#include <QString>
+
+namespace Calamares
+{
+DLLEXPORT const QString aboutStringUntranslated();
+DLLEXPORT const QString aboutString();
+}  // namespace Calamares
+
+#endif
