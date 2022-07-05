c91a73643824d840f827d9eaf589aed88fb0b247
shivanandvp <shivanandvp.oss@gmail.com>
2022-07-05 11:26:56 -0500

    feat: Remove suffix grouping in services-systemd

--- src/modules/services-systemd/main.py
+++ src/modules/services-systemd/main.py
@@ -7,6 +7,7 @@
 #   SPDX-FileCopyrightText: 2014 Teo Mrnjavac <teo@kde.org>
 #   SPDX-FileCopyrightText: 2017 Alf Gaida <agaida@siduction.org>
 #   SPDX-FileCopyrightText: 2018-2019 Adriaan de Groot <groot@kde.org>
+#   SPDX-FileCopyrightText: 2022 shivanandvp <shivanandvp@rebornos.org>
 #   SPDX-License-Identifier: GPL-3.0-or-later
 #
 #   Calamares is Free Software: see the License-Identifier above.
@@ -23,66 +24,59 @@ _ = gettext.translation("calamares-python",
 
 
 def pretty_name():
-    return _("Configure systemd services")
+    return _("Configure systemd units")
 
 
-def systemctl(targets, command, suffix):
+def systemctl(command, units):
     """
-    For each entry in @p targets, run "systemctl <command> <thing>",
-    where <thing> is the entry's name plus the given @p suffix.
-    (No dot is added between name and suffix; suffix may be empty)
+    For each entry in @p units, run "systemctl <command> <thing>",
+    where <thing> is the entry's full name.
 
     Returns a failure message, or None if this was successful.
-    Services that are not mandatory have their failures suppressed
+    Units that are not mandatory have their failures suppressed
     silently.
     """
-    for svc in targets:
-        if isinstance(svc, str):
-            name = svc
+    for unit in units:
+        if isinstance(unit, str):
+            name = unit
             mandatory = False
         else:
-            name = svc["name"]
-            mandatory = svc.get("mandatory", False)
+            name = unit["name"]
+            mandatory = unit.get("mandatory", False)
 
-        ec = libcalamares.utils.target_env_call(
-            ['systemctl', command, "{}{}".format(name, suffix)]
+        exit_code = libcalamares.utils.target_env_call(
+            ['systemctl', command, name]
             )
 
-        if ec != 0:
+        if exit_code != 0:
             libcalamares.utils.warning(
-                "Cannot {} systemd {} {}".format(command, suffix, name)
+                "Cannot {} systemd unit {}".format(command, name)
                 )
             libcalamares.utils.warning(
-                "systemctl {} call in chroot returned error code {}".format(command, ec)
+                "systemctl {} call in chroot returned error code {}".format(command, exit_code)
                 )
             if mandatory:
-                title = _("Cannot modify service")
-                diagnostic = _("<code>systemctl {arg!s}</code> call in chroot returned error code {num!s}.").format(arg=command, num=ec)
+                title = _("Cannot modify unit")
+                diagnostic = _("<code>systemctl {arg!s}</code> call in chroot returned error code {num!s}.").format(arg=command, num=exit_code)
 
-                if command == "enable" and suffix == ".service":
-                    description = _("Cannot enable systemd service <code>{name!s}</code>.")
-                elif command == "enable" and suffix == ".target":
-                    description = _("Cannot enable systemd target <code>{name!s}</code>.")
-                elif command == "enable" and suffix == ".timer":
-                    description = _("Cannot enable systemd timer <code>{name!s}</code>.")
-                elif command == "disable" and suffix == ".service":
-                    description = _("Cannot enable systemd service <code>{name!s}</code>.")
-                elif command == "disable" and suffix == ".target":
-                    description = _("Cannot disable systemd target <code>{name!s}</code>.")
+                if command == "enable":
+                    description = _("Cannot enable systemd unit <code>{name!s}</code>.")
+                elif command == "disable":
+                    description = _("Cannot disable systemd unit <code>{name!s}</code>.")
                 elif command == "mask":
                     description = _("Cannot mask systemd unit <code>{name!s}</code>.")
                 else:
-                    description = _("Unknown systemd commands <code>{command!s}</code> and <code>{suffix!s}</code> for unit {name!s}.")
+                    description = _("Unknown systemd commands <code>{command!s}</code> for unit {name!s}.")
 
                 return (title,
-                        description.format(name=name, command=command, suffix=suffix) + " " + diagnostic
+                        description.format(name=name, command=command) + " " + diagnostic
                         )
     return None
 
 
 def run():
     """
-    Setup systemd services
+    Setup systemd units
     """
     cfg = libcalamares.job.configuration
 
@@ -91,27 +85,15 @@ def run():
     # that support that, see:
     # http://0pointer.de/blog/projects/changing-roots.html
 
-    r = systemctl(cfg.get("services", []), "enable", ".service")
+    r = systemctl("enable", cfg.get("enable", []))
     if r is not None:
         return r
 
-    r = systemctl(cfg.get("targets", []), "enable", ".target")
+    r = systemctl("disable", cfg.get("disable", []))
     if r is not None:
         return r
 
-    r = systemctl(cfg.get("timers", []), "enable", ".timer")
-    if r is not None:
-        return r
-
-    r = systemctl(cfg.get("disable", []), "disable", ".service")
-    if r is not None:
-        return r
-
-    r = systemctl(cfg.get("disable-targets", []), "disable", ".target")
-    if r is not None:
-        return r
-
-    r = systemctl(cfg.get("mask", []), "mask", "")
+    r = systemctl("mask", cfg.get("mask", []))
     if r is not None:
         return r
 
