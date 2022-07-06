2afa0766e769bb01c8e84236d52796a6f7859dbe
shivanandvp <shivanandvp.oss@gmail.com>
2022-07-05 22:09:48 -0500

    feat: Make systemd module more generic

--- src/modules/services-systemd/main.py
+++ src/modules/services-systemd/main.py
@@ -11,7 +11,6 @@
 #   SPDX-License-Identifier: GPL-3.0-or-later
 #
 #   Calamares is Free Software: see the License-Identifier above.
-#
 
 import libcalamares
 
@@ -27,74 +26,58 @@ def pretty_name():
     return _("Configure systemd units")
 
 
-def systemctl(command, units):
+def systemctl(units):
     """
-    For each entry in @p units, run "systemctl <command> <thing>",
-    where <thing> is the entry's full name.
+    For each entry in @p units, run "systemctl <action> <name>",
+    where each unit is a map of unit name, action, and a flag.
 
     Returns a failure message, or None if this was successful.
     Units that are not mandatory have their failures suppressed
     silently.
     """
+
     for unit in units:
         if isinstance(unit, str):
             name = unit
+            action = "enable"
             mandatory = False
         else:
             name = unit["name"]
+            action = unit.get("action", "enable")
             mandatory = unit.get("mandatory", False)
 
         exit_code = libcalamares.utils.target_env_call(
-            ['systemctl', command, name]
-            )
+            ['systemctl', action, name]
+        )
 
         if exit_code != 0:
             libcalamares.utils.warning(
-                "Cannot {} systemd unit {}".format(command, name)
-                )
+                "Cannot {} systemd unit {}".format(action, name)
+            )
             libcalamares.utils.warning(
-                "systemctl {} call in chroot returned error code {}".format(command, exit_code)
-                )
+                "systemctl {} call in chroot returned error code {}".format(action, exit_code)
+            )
             if mandatory:
-                title = _("Cannot modify unit")
-                diagnostic = _("<code>systemctl {arg!s}</code> call in chroot returned error code {num!s}.").format(arg=command, num=exit_code)
-
-                if command == "enable":
-                    description = _("Cannot enable systemd unit <code>{name!s}</code>.")
-                elif command == "disable":
-                    description = _("Cannot disable systemd unit <code>{name!s}</code>.")
-                elif command == "mask":
-                    description = _("Cannot mask systemd unit <code>{name!s}</code>.")
-                else:
-                    description = _("Unknown systemd commands <code>{command!s}</code> for unit {name!s}.")
-
-                return (title,
-                        description.format(name=name, command=command) + " " + diagnostic
-                        )
+                title = _("Cannot modify service")
+                diagnostic = _("<code>systemctl {_action!s}</code> call in chroot returned error code {_exit_code!s}.").format(_action=action, _exit_code=exit_code)
+                description = _("Cannot {_action!s} systemd unit <code>{_name!s}</code>.").format(_action=action, _name=name)
+                return (
+                    title,
+                    description + " " + diagnostic
+                )
     return None
 
 
 def run():
     """
-    Setup systemd units
+    Setup systemd services
     """
     cfg = libcalamares.job.configuration
 
-    # note that the "systemctl enable" and "systemctl disable" commands used
-    # here will work in a chroot; in fact, they are the only systemctl commands
-    # that support that, see:
-    # http://0pointer.de/blog/projects/changing-roots.html
-
-    r = systemctl("enable", cfg.get("enable", []))
-    if r is not None:
-        return r
-
-    r = systemctl("disable", cfg.get("disable", []))
-    if r is not None:
-        return r
-
-    r = systemctl("mask", cfg.get("mask", []))
-    if r is not None:
-        return r
+    return_value = systemctl(
+        cfg.get("units", [])
+    )
+    if return_value is not None:
+        return return_value
 
     return None
