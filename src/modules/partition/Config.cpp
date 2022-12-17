07f0b59bb161100dded3f4c5426081f79bf1fbe1
Adriaan de Groot <groot@kde.org>
2022-12-18 00:58:44 +0100

    [partition] Assign the replace-mode FS when reading the configuration

--- src/modules/partition/Config.cpp
+++ src/modules/partition/Config.cpp
@@ -375,7 +375,9 @@ Config::fillConfigurationFSTypes( const QVariantMap& configurationMap )
     Q_ASSERT( !m_eraseFsTypes.isEmpty() );
     Q_ASSERT( m_eraseFsTypes.contains( fsRealName ) );
     m_eraseFsTypeChoice = fsRealName;
+    m_replaceFileSystemChoice = fsRealName;
     Q_EMIT eraseModeFilesystemChanged( m_eraseFsTypeChoice );
+    Q_EMIT replaceModeFilesystemChanged( m_replaceFileSystemChoice );
 }
 
 void
