1b9d8b1f9189a416582818aeb0b926e3a4e2d9e2
Adriaan de Groot <groot@kde.org>
2023-09-10 20:23:06 +0200

    CI: tweak opensuse-qt6 nightly build
    Split the install-dependencies off into a shell script instead of
    being part of the workflow, so that it can be run manually or
    by other means than the GH workflow.

---
+++ .github/workflows/nightly-opensuse-qt6.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+# 
+# Install dependencies for the nightly-opensuse-qt6 build
+#
+# Add a Qt6/KF6 repo
+zypper --non-interactive addrepo -G https://download.opensuse.org/repositories/home:krop:kf6/openSUSE_Tumbleweed/home:krop:kf6.repo
+zypper --non-interactive refresh
+zypper --non-interactive up
+zypper --non-interactive in git-core jq curl
+# From deploycala.py
+zypper --non-interactive in bison flex git make cmake gcc-c++
+zypper --non-interactive in yaml-cpp-devel libpwquality-devel parted-devel python-devel libboost_headers-devel libboost_python3-devel
+zypper --non-interactive in libicu-devel libatasmart-devel
+# Qt6/KF6 dependencies
+zypper --non-interactive in qt6-concurrent-devel qt6-gui-devel qt6-linguist-devel qt6-network-devel qt6-svg-devel qt6-declarative-devel
+zypper --non-interactive in kf6-kcoreaddons-devel kf6-kdbusaddons-devel kf6-kcrash-devel
+zypper --non-interactive in libpolkit-qt6-1-devel
