546a4d0bbb37a0d8edadcfe2de09694617a16eda
Adriaan de Groot <groot@kde.org>
2023-09-11 22:02:08 +0200

    CI: switch opensuse-qt6 away from boost

--- ci/deps-opensuse-qt6.sh
+++ ci/deps-opensuse-qt6.sh
@@ -11,7 +11,7 @@ zypper --non-interactive up
 zypper --non-interactive in git-core jq curl ninja
 # From deploycala.py
 zypper --non-interactive in bison flex git make cmake gcc-c++
-zypper --non-interactive in yaml-cpp-devel libpwquality-devel parted-devel python-devel libboost_headers-devel libboost_python3-devel
+zypper --non-interactive in yaml-cpp-devel libpwquality-devel parted-devel python3-devel
 zypper --non-interactive in libicu-devel libatasmart-devel
 # Qt6/KF6 dependencies
 zypper --non-interactive in qt6-concurrent-devel qt6-gui-devel qt6-linguist-devel qt6-network-devel qt6-svg-devel qt6-declarative-devel
