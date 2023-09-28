445f1c0f56047697de6cba3519102a277aa16bee
Adriaan de Groot <groot@kde.org>
2023-09-28 22:22:43 +0200

    CI: script-tidying

--- ci/deps-opensuse.sh
+++ ci/deps-opensuse.sh
@@ -6,37 +6,37 @@ zypper --non-interactive up
 zypper --non-interactive in git-core jq curl
 # From deploycala.py
 zypper --non-interactive in \
-    "bison" \
-    "flex" \
-    "git" \
-    "make" \
-    "cmake" \
-    "gcc-c++"
+	"bison" \
+	"flex" \
+	"git" \
+	"make" \
+	"cmake" \
+	"gcc-c++"
 zypper --non-interactive in \
-    "libqt5-qtbase-devel" \
-    "libqt5-linguist-devel" \
-    "libqt5-qtsvg-devel" \
-    "libqt5-qtdeclarative-devel" \
-    "libqt5-qtwebengine-devel" \
-    "yaml-cpp-devel" \
-    "libpolkit-qt5-1-devel" \
-    "libpwquality-devel" \
-    "parted-devel" \
-    "python-devel" \
-    "libboost_headers-devel" \
-    "libboost_python3-devel"
+	"libqt5-qtbase-devel" \
+	"libqt5-linguist-devel" \
+	"libqt5-qtsvg-devel" \
+	"libqt5-qtdeclarative-devel" \
+	"libqt5-qtwebengine-devel" \
+	"yaml-cpp-devel" \
+	"libpolkit-qt5-1-devel" \
+	"libpwquality-devel" \
+	"parted-devel" \
+	"python-devel" \
+	"libboost_headers-devel" \
+	"libboost_python3-devel"
 zypper --non-interactive in \
-    "extra-cmake-modules" \
-    "kdbusaddons-devel" \
-    "kservice-devel" \
-    "kpackage-devel" \
-    "kparts-devel" \
-    "kcrash-devel" \
-    "kpmcore-devel" \
-    "plasma5-workspace-devel" \
-    "plasma-framework-devel"
+	"extra-cmake-modules" \
+	"kdbusaddons-devel" \
+	"kservice-devel" \
+	"kpackage-devel" \
+	"kparts-devel" \
+	"kcrash-devel" \
+	"kpmcore-devel" \
+	"plasma5-workspace-devel" \
+	"plasma-framework-devel"
 # Additional dependencies
 zypper --non-interactive in \
-    libicu-devel \
-    libAppStreamQt-devel \
-    libatasmart-devel
+	libicu-devel \
+	libAppStreamQt-devel \
+	libatasmart-devel
