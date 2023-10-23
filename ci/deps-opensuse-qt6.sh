aee0786c22818ff32600ad01b13b21d33c80d435
Christophe Marin <christophe@krop.fr>
2023-10-23 09:16:39 +0200

    CI: change the openSUSE Qt6 repository
    home:krop:Qt6 is only used to prepare new releases and disabled afterwards.
    Use KDE:Qt6 instead.

--- ci/deps-opensuse-qt6.sh
+++ ci/deps-opensuse-qt6.sh
@@ -4,7 +4,7 @@
 #
 # Add a Qt6/KF6 repo
 zypper --non-interactive addrepo -G https://download.opensuse.org/repositories/home:krop:kf6/openSUSE_Tumbleweed/home:krop:kf6.repo
-zypper --non-interactive addrepo -G https://download.opensuse.org/repositories/home:/krop:/Qt6:/Release/openSUSE_Tumbleweed/home:krop:Qt6:Release.repo
+zypper --non-interactive addrepo -G https://download.opensuse.org/repositories/KDE:/Qt6/openSUSE_Tumbleweed/KDE:Qt6.repo
 
 zypper --non-interactive refresh
 zypper --non-interactive up
