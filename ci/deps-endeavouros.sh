cfadf8f95a6174ead36b1c22f7ba54b582e01733
Adriaan de Groot <groot@kde.org>
2023-11-07 00:11:52 +0100

    CI: add a -deps shell script for EndeavourOS

---
+++ ci/deps-endeavouros.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+#
+# Install dependencies for building on EndeavourOS
+#
+# There is no docker image for EndeavoudOS, and the live ISO
+# for Cassini Nova is KF5 / Qt5 based, but we can build there.
+# It even has most of the build-deps already installed.
+pacman -Syu --noconfirm git cmake ninja jq
+pacman -S --noconfirm gcc yaml-cpp icu
+pacman -S --noconfigm extra-cmake-modules
+
