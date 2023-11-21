5809b32c98c968bb519e0cda76e16c4bb8251480
Adriaan de Groot <groot@kde.org>
2023-11-21 15:11:59 +0100

    CI: expand fedora dependencies

--- ci/deps-fedora-qt6.sh
+++ ci/deps-fedora-qt6.sh
@@ -8,7 +8,8 @@ yum install -y yaml-cpp-devel libpwquality-devel parted-devel python-devel gette
 yum install -y libicu-devel libatasmart-devel
 # Qt6/KF6 dependencies
 yum install -y qt6-qtbase-devel qt6-linguist qt6-qtbase-private-devel qt6-qtdeclarative-devel qt6-qtsvg-devel qt6-qttools-devel
-yum install -y kf6-kcoreaddons-devel kf6-kdbusaddons-devel kf6-kcrash-devel
+yum install -y extra-cmake-modules kf6-kcoreaddons-devel kf6-kdbusaddons-devel kf6-kcrash-devel
+yum install -y kf6-kconfig-devel kf6-ki18n-devel kf6-kwidgetsaddons-devel kf6-kservice-devel
 yum install -y polkit-qt6-1-devel appstream-qt-devel
 # Runtime dependencies for QML modules
 yum install -y kf6-kirigami2-devel || true
