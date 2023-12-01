e2899ffa1d43ef99bddb07388d8282b457c99289
Adriaan de Groot <groot@kde.org>
2023-12-02 00:28:54 +0100

    CI: ensure the right ECM

--- ci/deps-opensuse-qt6.sh
+++ ci/deps-opensuse-qt6.sh
@@ -14,6 +14,7 @@ zypper --non-interactive in bison flex git make cmake gcc-c++
 zypper --non-interactive in yaml-cpp-devel libpwquality-devel parted-devel python3-devel
 zypper --non-interactive in libicu-devel libatasmart-devel
 # Qt6/KF6 dependencies
+zypper --non-interactive in kf6-extra-cmake-modules
 zypper --non-interactive in "qt6-declarative-devel" "cmake(Qt6Concurrent)" "cmake(Qt6Gui)" "cmake(Qt6Network)" "cmake(Qt6Svg)" "cmake(Qt6Linguist)"
 zypper --non-interactive in "cmake(KF6CoreAddons)" "cmake(KF6DBusAddons)" "cmake(KF6Crash)"
 zypper --non-interactive in "cmake(KF6Parts)" # Also installs KF5 things
