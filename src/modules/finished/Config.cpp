810f8d708174a83ef4167c8cd6aeb27c32c6f3eb
Adriaan de Groot <groot@kde.org>
2024-07-02 17:31:39 +0200

    [finished] Sketch the way configuration is parsed now

--- src/modules/finished/Config.cpp
+++ src/modules/finished/Config.cpp
@@ -118,8 +118,94 @@ Config::doNotify( bool hasFailed, bool sendAnyway )
     }
 }
 
+using Choices = int;
+
+static Choices loadLive(bool isSelected)
+{
+    return isSelected ? 1 : 0;
+}
+
+static Choices loadRestart(bool isSelected, const QString & /* command */ )
+{
+    return isSelected ? 1 : 0;
+}
+
+static Choices loadFromV3(const QVariantList & items, const QString & defaultChoice )
+{
+    return 0;
+}
+
+static Choices loadFromV2(const QString & restartNowMode, const QString & restartNowCommand)
+{
+    if (restartNowMode == QStringLiteral( "never"))
+    {
+        return loadLive(true);
+    }
+
+    // Empty if not specified, defaults to user-unchecked according to the suggested config
+    if (restartNowMode == QStringLiteral("user-unchecked") || restartNowMode == QStringLiteral("unchecked") || restartNowMode.isEmpty())
+    {
+        Choices c = loadLive(true);
+        c += loadRestart(false, restartNowCommand);
+        return c;
+    }
+
+    if (restartNowMode == QStringLiteral("user-checked") || restartNowMode == QStringLiteral("checked"))
+    {
+        Choices c = loadLive(false);
+        c += loadRestart(true, restartNowCommand);
+        return c;
+    }
+
+    if (restartNowMode == QStringLiteral("always"))
+    {
+        return loadRestart(true, restartNowCommand);
+    }
+
+    cError() << "Unknown v2 restart mode" << restartNowMode << "using 'never'";
+    return loadLive(true);
+}
+
+static Choices loadFromV1(bool restartNowEnabled, bool restartNowChecked, const QString & restartNowCommand)
+{
+    if (!restartNowEnabled)
+    {
+        return loadFromV2( QStringLiteral("never"), QString());
+    }
+    if (restartNowChecked)
+    {
+        return loadFromV2( QStringLiteral("user-checked"), restartNowCommand);
+    }
+    else
+    {
+        return loadFromV2( QStringLiteral("user-unchecked"), restartNowCommand);
+    }
+}
+
+
 void
 Config::setConfigurationMap( const QVariantMap& configurationMap )
 {
     m_notifyOnFinished = Calamares::getBool( configurationMap, "notifyOnFinished", false );
+
+    const auto v1Enabled = QStringLiteral( "restartNowEnabled" );
+    const auto v1Checked = QStringLiteral( "restartNowChecked" );
+
+    const auto v2Mode = QStringLiteral("restartNowMode");
+    const auto v2Command = QStringLiteral("restartNowCommand");
+
+    if(configurationMap.contains( v1Enabled) || configurationMap.contains(v1Checked))
+    {
+        cWarning() << "Configuration uses v1 settings" << v1Enabled << "and" << v1Checked;
+        loadFromV1(Calamares::getBool(configurationMap, v1Enabled, true), Calamares::getBool(configurationMap, v1Checked, false), Calamares::getString(configurationMap, v2Command));
+    }
+    else if(configurationMap.contains( v2Mode) || configurationMap.contains(v2Command))
+    {
+        cWarning() << "Configuration uses v2 settings" << v2Mode << "and" << v2Command;
+        loadFromV2(Calamares::getString(configurationMap, v2Mode, QStringLiteral("user-unchecked")), Calamares::getString(configurationMap, v2Command));
+    }
+    else
+    {
+        loadFromV3(Calamares::getList(configurationMap, QStringLiteral("choices")), Calamares::getString(configurationMap, QStringLiteral("defaultChoice")));
+    }
 }
