7f49b21c49ea4659e2d79dda954640b84edd53fc
Adriaan de Groot <groot@kde.org>
2024-09-08 07:06:39 -0400

    CI: add tools for schema-validation to EndeavourOS build

--- ci/deps-endeavouros.sh
+++ ci/deps-endeavouros.sh
@@ -5,7 +5,9 @@
 # There is no docker image for EndeavoudOS, and the live ISO
 # for Cassini Nova is KF5 / Qt5 based, but we can build there.
 # It even has most of the build-deps already installed.
-pacman -Syu --noconfirm git cmake ninja jq
-pacman -S --noconfirm gcc yaml-cpp icu
-pacman -S --noconfirm extra-cmake-modules
+pacman -Syu --noconfirm jq
+pacman -S --noconfirm git cmake ninja jq || exit 1
+pacman -S --noconfirm gcc yaml-cpp icu || exit 1
+pacman -S --noconfirm extra-cmake-modules || exit 1
+pacman -S --noconfirm python-jsonschema || exit 1
 
